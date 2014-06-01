4a9641b363b3816a01aa0989942791afa780a5fb
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-06-01 15:50:48 +0000

    Added all widgets to the widget folder.

---
+++ plugins/so-widgets-bundle/trunk/widgets/so-price-table-widget/so-price-table-widget.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Price Table Widget
+Description: A powerful yet simple price table widget for your sidebars or Page Builder pages.
+Version: trunk
+Author: Greg Priday
+Author URI: http://siteorigin.com
+Plugin URI: http://siteorigin.com/price-table-widget/
+License: GPL3
+License URI: license.txt
+*/
+
+return new SiteOrigin_Widgets_Loader('price-table', __FILE__, plugin_dir_path(__FILE__).'inc/widget.php');
