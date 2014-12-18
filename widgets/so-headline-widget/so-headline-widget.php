e50ee76b892a843caa47b2a1e8bd6c67a798e17c
Braam Genis <braamgenis@gmail.com>
2014-12-18 19:49:41 +0200

    template and styling for headline widget.

--- widgets/so-headline-widget/so-headline-widget.php
+++ widgets/so-headline-widget/so-headline-widget.php
@@ -8,8 +8,18 @@ Author URI: http://siteorigin.com
 */
 
 class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
+
+	static $web_safe = array(
+		'Helvetica Neue' => 'Arial, Helvetica, Geneva, sans-serif',
+		'Lucida Grande' => 'Lucida, Verdana, sans-serif',
+		'Georgia' => '"Times New Roman", Times, serif',
+		'Courier New' => 'Courier, mono',
+	);
+
+	static $google_web_fonts;
+
 	function __construct() {
-		$google_web_fonts = include ( dirname(__FILE__) . '/data/google-fonts.php' );
+		self::$google_web_fonts = include ( dirname(__FILE__) . '/data/google-fonts.php' );
 
 		// Add the default fonts
 		$fonts = array(
@@ -19,7 +29,7 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 			'Courier New' => 'Courier New',
 		);
 
-		foreach ( $google_web_fonts as $font => $variants ) {
+		foreach ( self::$google_web_fonts as $font => $variants ) {
 			foreach ( $variants as $variant ) {
 				if ( $variant == 'regular' || $variant == 400 ) {
 					$fonts[ $font ] = $font;
@@ -53,6 +63,10 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 							'label' => __( 'Font', 'siteorigin-widgets' ),
 							'options' => $fonts
 						),
+						'color' => array(
+							'type' => 'color',
+							'label' => __('Color', 'siteorigin-widgets')
+						),
 						'align' => array(
 							'type' => 'select',
 							'label' => __( 'Align', 'siteorigin-widgets' ),
@@ -79,6 +93,10 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 							'label' => __( 'Font', 'siteorigin-widgets' ),
 							'options' => $fonts
 						),
+						'color' => array(
+							'type' => 'color',
+							'label' => __('Color', 'siteorigin-widgets')
+						),
 						'align' => array(
 							'type' => 'select',
 							'label' => __( 'Align', 'siteorigin-widgets' ),
@@ -96,11 +114,51 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 	}
 
 	function get_style_name( $instance ) {
-		return '';
+		return 'sow-headline';
+	}
+
+	function get_less_variables( $instance ) {
+		$headline_font = $instance['headline']['font'];
+		$sub_headline_font = $instance['sub_headline']['font'];
+		if ( isset( self::$web_safe[ $headline_font ] ) ) {
+			$headline_font = self::$web_safe[ $headline_font ];
+		}
+		if ( isset( self::$web_safe[ $sub_headline_font ] ) ) {
+			$sub_headline_font = self::$web_safe[ $sub_headline_font ];
+		}
+		return array(
+			'headline_align' => $instance['headline']['align'],
+			'headline_color' => $instance['headline']['color'],
+			'headline_font' => $headline_font,
+			'sub_headline_align' => $instance['sub_headline']['align'],
+			'sub_headline_color' => $instance['sub_headline']['color'],
+			'sub_headline_font' => $sub_headline_font,
+		);
+	}
+
+	function less_import_google_font($instance, $args) {
+		$headline_font = $instance['headline']['font'];
+		$sub_headline_font = $instance['sub_headline']['font'];
+		$fonts = array();
+		if ( isset( self::$google_web_fonts[ $headline_font ] ) ) {
+			$fonts[] = $headline_font;
+		}
+		if ( isset( self::$google_web_fonts[ $sub_headline_font ] ) ) {
+			$fonts[] = $sub_headline_font;
+		}
+		$fams = implode( '|', $fonts );
+		return empty( $fonts ) ? '' : '@import url(http' . ( is_ssl() ? 's' : '' ) . '://fonts.googleapis.com/css?family=' . $fams . '); ';
 	}
 
 	function get_template_name( $instance ) {
 		return 'headline';
+	}
+
+	function get_template_variables( $instance, $args ) {
+		return array(
+			'headline' => $instance['headline']['text'],
+			'sub_headline' => $instance['sub_headline']['text'],
+		);
 	}
 }
 
---
+++ widgets/so-headline-widget/tpl/headline.php
@@ -0,0 +1,7 @@
+<div class="sow-headline">
+	<h1><?php echo $headline ?></h1>
+	<div class="decoration">
+		<div class="decoration-inside"></div>
+	</div>
+	<h3><?php echo $sub_headline ?></h3>
+</div>
