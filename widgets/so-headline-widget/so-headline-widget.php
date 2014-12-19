f0ae4fafd95574c4978e894757480c34f4282a2f
Braam Genis <braamgenis@gmail.com>
2014-12-19 15:50:30 +0200

    Headline widget:  - Import statements for google fonts.  - Handle google fonts correctly.

--- widgets/so-headline-widget/so-headline-widget.php
+++ widgets/so-headline-widget/so-headline-widget.php
@@ -56,7 +56,7 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 					'fields' => array(
 						'text' => array(
 							'type' => 'text',
-							'label' => __( 'Headline', 'siteorigin-wdigets' ),
+							'label' => __( 'Text', 'siteorigin-wdigets' ),
 						),
 						'font' => array(
 							'type' => 'select',
@@ -86,7 +86,7 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 					'fields' => array(
 						'text' => array(
 							'type' => 'text',
-							'label' => __('Sub headline', 'siteorigin-wdigets')
+							'label' => __('Text', 'siteorigin-wdigets')
 						),
 						'font' => array(
 							'type' => 'select',
@@ -119,35 +119,60 @@ class SiteOrigin_Widget_Headline_Widget extends SiteOrigin_Widget {
 
 	function get_less_variables( $instance ) {
 		$headline_font = $instance['headline']['font'];
-		$sub_headline_font = $instance['sub_headline']['font'];
+		$headline_font_weight = 400;
 		if ( isset( self::$web_safe[ $headline_font ] ) ) {
 			$headline_font = self::$web_safe[ $headline_font ];
 		}
+		else {
+			$font_parts = explode( ':', $headline_font )[0];
+			$headline_font = $font_parts[0];
+			if ( count( $font_parts ) > 1 ) {
+				$headline_font_weight = $font_parts[1];
+			}
+		}
+		$sub_headline_font = $instance['sub_headline']['font'];
+		$sub_headline_font_weight = 400;
 		if ( isset( self::$web_safe[ $sub_headline_font ] ) ) {
 			$sub_headline_font = self::$web_safe[ $sub_headline_font ];
+		}
+		else {
+			$sub_headline_font = explode( ':', $sub_headline_font )[0];
+			$sub_headline_font_weight = explode( ':', $sub_headline_font )[1];
+
+			$font_parts = explode( ':', $sub_headline_font )[0];
+			$sub_headline_font = $font_parts[0];
+			if ( count( $font_parts ) > 1 ) {
+				$sub_headline_font_weight = $font_parts[1];
+			}
 		}
 		return array(
 			'headline_align' => $instance['headline']['align'],
 			'headline_color' => $instance['headline']['color'],
 			'headline_font' => $headline_font,
+			'headline_font_weight' => $headline_font_weight,
 			'sub_headline_align' => $instance['sub_headline']['align'],
 			'sub_headline_color' => $instance['sub_headline']['color'],
 			'sub_headline_font' => $sub_headline_font,
+			'sub_headline_font_weight' => $sub_headline_font_weight,
 		);
 	}
 
 	function less_import_google_font($instance, $args) {
-		$headline_font = $instance['headline']['font'];
-		$sub_headline_font = $instance['sub_headline']['font'];
-		$fonts = array();
-		if ( isset( self::$google_web_fonts[ $headline_font ] ) ) {
-			$fonts[] = $headline_font;
+		$this->extract_google_font( $instance['headline']['font'], $fonts );
+		$this->extract_google_font( $instance['sub_headline']['font'], $fonts );
+
+		return empty( $fonts ) ? '' : '@import url(http' . ( is_ssl() ? 's' : '' ) . '://fonts.googleapis.com/css?family=' . implode( '|', $fonts ) . '); ';
+	}
+
+	private function extract_google_font($font, &$fonts) {
+		$font_parts = explode( ':', $font );
+		if ( isset( self::$google_web_fonts[ $font_parts[0] ] ) ) {
+			$gfont = urlencode( $font_parts[0] );
+			if ( count( $font_parts ) > 1 ) {
+				$gfont .= ':' . $font_parts[1];
+			}
+			$fonts[] = $gfont;
 		}
-		if ( isset( self::$google_web_fonts[ $sub_headline_font ] ) ) {
-			$fonts[] = $sub_headline_font;
-		}
-		$fams = implode( '|', $fonts );
-		return empty( $fonts ) ? '' : '@import url(http' . ( is_ssl() ? 's' : '' ) . '://fonts.googleapis.com/css?family=' . $fams . '); ';
 	}
 
 	function get_template_name( $instance ) {
