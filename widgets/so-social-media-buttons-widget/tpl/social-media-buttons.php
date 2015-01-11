1102833049fc0b258df1694a9bc7e1f160df2db1
gregpriday <greg@siteorigin.com>
2015-01-11 21:31:13 +0200

    Moved social links widget
    Moved social links widget from the experimental widgets bundle into the main bundle.

---
+++ widgets/so-social-media-buttons-widget/tpl/social-media-buttons.php
@@ -0,0 +1,22 @@
+<div class="social-media-button-container">
+	<?php foreach( $networks as $network ) :
+		$classes = array();
+		if( !empty($instance['design']['hover']) ) $classes[] = 'ow-button-hover';
+		$classes[] = "sow-social-media-button-" . sanitize_html_class( $network['name'] );
+		$classes[] = "sow-social-media-button";
+		$button_attributes = array(
+			'class' => esc_attr(implode(' ', $classes))
+		);
+		if(!empty($instance['design']['new_window'])) $button_attributes['target'] = '_blank';
+		if ( ! empty( $network['url'] ) ) $button_attributes['href'] = esc_url( $network['url'] );
+		?>
+
+		<a <?php foreach($button_attributes as $name => $val) echo $name . '="' . $val . '" ' ?>>
+			<span>
+				<!-- premium-<?php echo $network['name'] ?> -->
+				<?php echo siteorigin_widget_get_icon( $network['icon_name'] ); ?>
+				<!-- endpremium -->
+			</span>
+		</a>
+	<?php endforeach; ?>
+</div>
