9c7821cdfba63dd7c20540b6d4f65c7b760ebb7b
Braam Genis <braamgenis@gmail.com>
2015-01-28 09:48:34 +0200

    Use plugin_dir_path for script include.

--- widgets/so-masonry-widget/so-masonry-widget.php
+++ widgets/so-masonry-widget/so-masonry-widget.php
@@ -65,4 +65,4 @@ class SiteOrigin_Widget_Masonry_Widget extends SiteOrigin_Widget {
 }
 
 siteorigin_widget_register( 'masonry', __FILE__ );
-include_once( 'inc/masonry_post_settings.php' );
+include_once plugin_dir_path( __FILE__ ) . 'inc/masonry_post_settings.php';
