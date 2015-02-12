74382530b62f493f9e266e19c9c63fa23333214c
Braam Genis <braamgenis@gmail.com>
2015-02-13 00:23:56 +0200

    Testimonial widget: - default style with some basic design options.

--- widgets/so-testimonial-widget/so-testimonial-widget.php
+++ widgets/so-testimonial-widget/so-testimonial-widget.php
@@ -23,6 +23,7 @@ class SiteOrigin_Widget_Testimonial_Widget extends SiteOrigin_Widget  {
 					'type' => 'select',
 					'label' => __( 'Template', 'siteorigin-widgets' ),
 					'options' => array(
+						'default' => __( 'Default', 'siteorigin-widgets' ),
 						'simple-left' => __( 'Simple Image Left', 'siteorigin-widgets' ),
 						'simple-top' => __( 'Simple Image Top', 'siteorigin-widgets' ),
 					)
@@ -56,16 +57,75 @@ class SiteOrigin_Widget_Testimonial_Widget extends SiteOrigin_Widget  {
 					'type' => 'checkbox',
 					'label' => __('Open In New Window', 'siteorigin-widgets'),
 				),
+				'design' => array(
+					'type' => 'section',
+					'label' => __( 'Design and layout', 'siteorigin-widgets' ),
+					'hide' => true,
+					'description' => __( 'Style options for the default template.', 'siteorigin-widgets' ),
+					'fields' => array(
+						'show_border' => array(
+							'type' => 'checkbox',
+							'label' => __( 'Show border', 'siteorigin-widgets' ),
+							'default' => true
+						),
+						'background_color' => array(
+							'type'    => 'color',
+							'label'   => __( 'Background color', 'siteorigin-widgets' ),
+							'default' => '#FCFCFC'
+						),
+						'box_shadow' => array(
+							'type' => 'checkbox',
+							'label' => __( 'Show box shadow', 'siteorigin-widgets' ),
+							'default' => true,
+						)
+					)
+				),
 			)
 		);
 	}
 
 	function get_style_name( $instance ) {
-		return ! empty( $instance['template'] ) ? $instance['template'] : 'simple-left';
+		if ( empty( $instance['template'] ) || $instance['template'] == 'default' ) {
+			return 'default';
+		}
+		else {
+			return $instance['template'];
+		}
 	}
 
 	function get_template_name( $instance ) {
-		return ! empty( $instance['template'] ) ? $instance['template'] : 'simple-left';
+		return ! empty( $instance['template'] ) ? $instance['template'] : 'default';
+	}
+
+	function get_template_variables( $instance, $args ) {
+		if ( ! empty( $instance['image'] ) ) {
+			$image_src = wp_get_attachment_image_src( $instance['image'] );
+		}
+
+		return array(
+			'image_url' => ! empty( $image_src ) ? $image_src[0] : '',
+			'testimonial' => $instance['text'],
+			'has_url' => ! empty( $instance['url'] ),
+			'url' => $instance['url'],
+			'location' => $instance['location'],
+			'new_window' => $instance['new_window'],
+		);
+	}
+
+	function get_less_variables( $instance ) {
+		$border_style = '1px solid #D0D0D0';
+		if ( empty( $instance['design']['show_border'] ) ) {
+			$border_style = 'none';
+		}
+		$box_shadow = '0 1px 2px rgba(0,0,0,0.1)';
+		if ( empty($instance['design']['box_shadow'] ) ) {
+			$box_shadow = 'none';
+		}
+		return array(
+			'borders' => $border_style,
+			'background_color' => $instance['design']['background_color'],
+			'box_shadow' => $box_shadow
+		);
 	}
 }
 
---
+++ widgets/so-testimonial-widget/tpl/default.php
@@ -0,0 +1,15 @@
+<?php if ( ! empty( $image_url ) ) : ?>
+<div class="testimonial-image-wrapper">
+	<img src="<?php echo esc_url( $image_url ) ?>" />
+</div>
+<?php endif; ?>
+
+<div class="text">
+	<?php echo wpautop( wp_kses_post( $testimonial ) ); ?>
+	<h5 class="testimonial-name">
+		<?php if( $has_url ) : ?><a href="<?php echo esc_url( $url ) ?>" <?php if( $new_window ) : ?>target="_blank"<?php endif; ?>><?php endif; ?>
+			<?php echo esc_html($instance['name']) ?>
+		<?php if( $has_url ) : ?></a><?php endif; ?>
+	</h5>
+	<small class="testimonial-location"><?php echo esc_html( $location ) ?></small>
+</div>
