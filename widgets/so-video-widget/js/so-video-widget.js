1f50153f71303b2cf8b9112ef4ab6d94ac983013
Braam Genis <braamgenis@gmail.com>
2015-04-09 17:02:38 +0200

    574 Self hosted video - Made some headway into getting chromeless player.

--- widgets/so-video-widget/js/so-video-widget.js
+++ widgets/so-video-widget/js/so-video-widget.js
@@ -1,5 +1,18 @@
 jQuery(function ($) {
 
-    $('#player1').mediaelementplayer();
+    // For default skin use this.
+    var player = new MediaElementPlayer('#player1');
+    // For other skins use this with customized HTML buttons
+    //var player = new MediaElement('player1', {
+    //    enablePluginDebug: false,
+    //    success: function (media) {
+    //        console.log('SUCCESS!!!');
+    //        media.play();
+    //    },
+    //    error: function (error) {
+    //        console.log('FAIL!!!');
+    //        console.log(error);
+    //    }
+    //});
 
 });
--- widgets/so-video-widget/tpl/default.php
+++ widgets/so-video-widget/tpl/default.php
@@ -1,5 +1,7 @@
 <?php echo $args['before_title'] . esc_html($instance['title']) . $args['after_title'] ?>
 
-<video width="640" height="360" id="player1" preload="none" poster="<?php echo esc_attr( $psoter ) ?>" controls="none">
-	<source type="video/<?php echo $video_type ?>" src="<?php echo esc_attr( $src ) ?>" />
-</video>
+<div>
+	<video width="640" height="360" id="player1" preload="none" poster="<?php echo esc_attr( $poster ) ?>">
+		<source type="video/<?php echo $video_type ?>" src="<?php echo esc_attr( $src ) ?>" />
+	</video>
+</div>
