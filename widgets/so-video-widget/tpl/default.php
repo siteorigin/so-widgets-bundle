c9383fcb3e15d30e6bbde959a245a546080021e3
Braam Genis <braamgenis@gmail.com>
2015-04-10 16:23:20 +0200

    575 Source: YouTube - fixed odd issue with custom skin controls not appearing for YouTube video.

--- widgets/so-video-widget/tpl/default.php
+++ widgets/so-video-widget/tpl/default.php
@@ -2,7 +2,9 @@
 
 <div>
 <?php if ( $host_type == 'self' || $video_type == 'youtube' || $video_type == 'vimeo' ) : ?>
-	<video width="640" height="360" id="<?php echo $player_id ?>" preload="none" poster="<?php echo esc_attr( $poster ) ?>"
+	<video width="640" height="360" id="<?php echo $player_id ?>" preload="none"
+	       poster="<?php echo esc_attr( $poster ) ?>"
+		<?php if( ! empty( $poster ) ) echo 'poster="' . $poster . '"' ?>
 		<?php if( $skin_class != 'default' ) echo 'class="mejs-' . $skin_class . '"' ?>>
 		<source type="video/<?php echo $video_type ?>" src="<?php echo esc_attr( $src ) ?>" />
 	</video>
