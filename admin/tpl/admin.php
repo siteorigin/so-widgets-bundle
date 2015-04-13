8302df0672a9d05aca4e98664274a6a99a2b43cb
Greg Priday <greg@siteorigin.com>
2015-04-13 17:50:29 +0200

    Refreshed widget bundle settings page.
    Still need to implement the tab interface and search.

--- admin/tpl/admin.php
+++ admin/tpl/admin.php
@@ -1,7 +1,27 @@
-<div class="wrap">
-	<h2><?php _e('SiteOrigin Widgets', 'siteorigin-widgets'); ?></h2>
+<div class="wrap" id="sow-widgets-page">
+	<div class="page-banner">
 
-	<div id="siteorigin-widgets-bundle">
+		<span class="icon">
+			<img src="<?php echo plugin_dir_url(__FILE__) ?>../images/icon-back.png" class="icon-back" width="50" height="43">
+			<img src="<?php echo plugin_dir_url(__FILE__) ?>../images/icon-gear.png" class="icon-gear" width="26" height="26">
+			<img src="<?php echo plugin_dir_url(__FILE__) ?>../images/icon-front.png" class="icon-front" width="50" height="43">
+		</span>
+		<h1><?php _e('SiteOrigin Widgets Bundle', 'siteorigin-widgets') ?></h1>
+
+		<div id="sow-widget-search">
+			<input type="search" placeholder="<?php esc_attr_e('Search Widgets', 'siteorigin-widgets') ?>" />
+		</div>
+	</div>
+
+	<ul class="page-nav">
+		<li><a href="#">Test Section</a></li>
+		<li><a href="#">Test Section</a></li>
+		<li><a href="#">Test Section</a></li>
+	</ul>
+
+
+	<div id="widgets-list">
+
 		<?php foreach( $widgets as $id => $widget ): ?>
 			<div class="so-widget-wrap">
 				<div class="so-widget so-widget-is-<?php echo $widget['Active'] ? 'active' : 'inactive' ?>">
@@ -14,7 +34,7 @@
 					$banner = apply_filters('siteorigin_widgets_widget_banner', $banner, $widget);
 					?>
 
-					<img src="<?php echo sow_esc_url( !empty($banner) ? $banner : plugin_dir_url(__FILE__) . '../banners/default.png' ) ?>" />
+					<img src="<?php echo sow_esc_url( !empty($banner) ? $banner : plugin_dir_url(__FILE__) . '../../banners/default.png' ) ?>" />
 
 					<div class="so-widget-text">
 						<label class="switch">
