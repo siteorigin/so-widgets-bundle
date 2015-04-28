f021a2d0715990fe3cede0cce48c71edb110d0b7
Braam Genis <braamgenis@gmail.com>
2015-04-28 14:36:01 +0200

    640 Retrieve widget post meta - Added method for widgets to retrieve their meta data. - Added PHPDoc comments to SiteOrigin_Widget_Meta_Box_Manager

--- base/inc/siteorigin-widget-meta-box-manager.php
+++ base/inc/siteorigin-widget-meta-box-manager.php
@@ -2,7 +2,17 @@
 
 class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 
+	const POST_META_KEY = 'siteorigin-widgets-post-meta';
+
+	/**
+	 * @var array Fields which have been added for each widget.
+	 */
 	private $widget_form_fields;
+
+	/**
+	 * @var array Post types for which fields have been added.
+	 */
+	private $post_types;
 
 	/**
 	 * Get the single instance.
@@ -16,7 +26,10 @@ class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 		return $single;
 	}
 
-	function __construct() {
+	/**
+	 * Private
+	 */
+	public function __construct() {
 		parent::__construct(
 			'sow-meta-box-manager',
 			__('SiteOrigin Meta Box Manager', 'siteorigin-widgets'),
@@ -27,16 +40,24 @@ class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 			array(),
 			array()
 		);
+	}
+
+	function initialize() {
+		// Initialize number for field name attributes.
 		$this->number = 1;
+		$this->post_types = array();
+
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
 		add_action( 'save_post', array( $this, 'save_widget_post_meta' ), 10, 3 );
 	}
 
 	/**
+	 * This handles the 'add_meta_boxes' action. It merges widget fields into the form_options array before adding the
+	 * meta box
+	 *
 	 * @param $post_type
 	 */
 	public function add_meta_boxes( $post_type ) {
-		if ( ! is_admin() ) return;
 
 		$this->form_options = array();
 		foreach ( $this->widget_form_fields as $widget_id => $form_fields ) {
@@ -60,10 +81,11 @@ class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 	}
 
 	/**
+	 * This is the callback used by add_meta_box to render the widgets meta box form and enqueue any necessary scripts.
+	 *
 	 * @param $post
-	 * @param $meta_box
 	 */
-	public function render_widgets_meta_box( $post, $meta_box ) {
+	public function render_widgets_meta_box( $post ) {
 		wp_enqueue_script(
 			'sow-meta-box-manager-js',
 			plugin_dir_url(SOW_BUNDLE_BASE_FILE).'base/js/meta-box-manager' . SOW_BUNDLE_JS_SUFFIX . '.js',
@@ -71,27 +93,45 @@ class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 			SOW_BUNDLE_VERSION,
 			true
 		);
-		$widget_post_meta = get_post_meta( $post->ID, 'siteorigin-widgets-post-meta', true );
+		$widget_post_meta = get_post_meta( $post->ID, self::POST_META_KEY, true );
 		$this->form( $widget_post_meta );
 		?><input type="hidden" id="widget_post_meta" name="widget_post_meta"> <?php
 		wp_nonce_field( 'widget_post_meta_save', '_widget_post_meta_nonce' );
 	}
 
 	/**
+	 * This method should be called by any widgets that want to be able to store post meta data.
+	 *
 	 * @param string $widget_id Base id of the widget adding the fields.
 	 * @param array $fields The fields to add.
 	 * @param string|array $post_types A post type string, 'all' or an array of post types
 	 */
-	function append_to_form( $widget_id, $fields, $post_types = 'all' ) {
+	public function append_to_form( $widget_id, $fields, $post_types = 'all' ) {
 		if( empty( $fields ) || empty( $post_types ) ) return;
 
+		if( $post_types == 'all' ) {
+			$post_types = array( 'all' );
+		}
+
+		foreach( $post_types as $post_type ) {
+			if( !in_array( $post_type, $this->post_types ) ) {
+				$this->post_types[] = $post_type;
+			}
+		}
+
 		$this->widget_form_fields[$widget_id] = array(
-			'post_types' => $post_types == 'all' ? array( 'all' ) : $post_types,
+			'post_types' =>  $post_types,
 			'fields' => $fields
 		);
 	}
 
-	function save_widget_post_meta( $post_id ) {
+	/**
+	 * This handles the 'save_post' action. It checks for a nonce, checks user permissions, and filters the input data
+	 * and decodes it from the JSON format before storing it in the post's meta data.
+	 *
+	 * @param $post_id
+	 */
+	public function save_widget_post_meta( $post_id ) {
 		$nonce = filter_input( INPUT_POST, '_widget_post_meta_nonce', FILTER_SANITIZE_STRING );
 		if ( !wp_verify_nonce( $nonce, 'widget_post_meta_save' ) ) return;
 		if ( !current_user_can( 'edit_post', $post_id ) ) return;
@@ -101,14 +141,42 @@ class SiteOrigin_Widget_Meta_Box_Manager extends SiteOrigin_Widget {
 		) );
 		$widget_post_meta = json_decode( $request['widget_post_meta'], true);
 
-		update_post_meta( $post_id, 'siteorigin-widgets-post-meta', $widget_post_meta );
+		update_post_meta( $post_id, self::POST_META_KEY, $widget_post_meta );
 
 	}
 
+	/**
+	 * This function is used to retrieve a widget's post meta data.
+	 *
+	 * @param $post_id string The id of the post for which the meta data is stored.
+	 * @param $widget_id string The id of the widget for which the meta data is stored.
+	 * @param $meta_key string The key of the meta data value which is to be retrieved.
+	 * @return mixed An empty string if the meta data is not found, else the meta data in whatever format it was stored.
+	 */
+	public function get_widget_post_meta( $post_id, $widget_id, $meta_key ) {
+		$widget_post_meta = get_post_meta( $post_id, self::POST_META_KEY, true );
+		if( empty( $widget_post_meta ) ) return '';
+		$widget_post_meta_field = $widget_id . '_' . $meta_key;
+		if( ! isset( $widget_post_meta[$widget_post_meta_field] ) ) return '';
+		return $widget_post_meta[$widget_post_meta_field];
+	}
+
+	/**
+	 * Unused override.
+	 *
+	 * @param $instance
+	 * @return string
+	 */
 	function get_style_name( $instance ) {
 		return '';
 	}
 
+	/**
+	 * Unused override.
+	 *
+	 * @param $instance
+	 * @return string
+	 */
 	function get_template_name( $instance ) {
 		return '';
 	}
