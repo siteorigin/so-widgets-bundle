ff8afd46dece4309aaeaf16bb4475665ff8bd9dd
Braam Genis <braamgenis@gmail.com>
2015-05-01 16:25:01 +0200

    673 Create select field class.

---
+++ base/inc/fields/siteorigin-widget-field-select.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Class SiteOrigin_Widget_Field_Select
+ */
+class SiteOrigin_Widget_Field_Select extends SiteOrigin_Widget_Field {
+	/**
+	 * The list of options which may be selected.
+	 *
+	 * @access protected
+	 * @var array
+	 */
+	protected $options;
+	/**
+	 * If present, this string is included as a disabled (not selectable) value at the top of the list of options. If
+	 * there is no default value, it is selected by default. You might even want to leave the label value blank when
+	 * you use this.
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $prompt;
+
+	public function __construct( $base_name, $element_id, $element_name, $options ) {
+		parent::__construct( $base_name, $element_id, $element_name, $options );
+
+		if( isset( $options['options'] ) ) $this->options = $options['options'];
+		if( isset( $options['prompt'] ) ) $this->prompt = $options['prompt'];
+	}
+
+	protected function render_field( $value ) {
+		?>
+		<select name="<?php echo $this->element_name ?>" id="<?php echo $this->element_id ?>"
+		        class="siteorigin-widget-input">
+			<?php if ( isset( $this->prompt ) ) : ?>
+				<option value="default" disabled="disabled" selected="selected"><?php echo esc_html( $this->prompt ) ?></option>
+			<?php endif; ?>
+
+			<?php if( isset( $this->options ) && !empty( $this->options ) ) : ?>
+				<?php foreach( $this->options as $key => $val ) : ?>
+					<option value="<?php echo esc_attr( $key ) ?>" <?php selected( $key, $value ) ?>><?php echo esc_html( $val ) ?></option>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</select>
+		<?php
+	}
+
+	protected function sanitize_field_input( $value ) {
+		$sanitized_value = $value;
+		$keys = array_keys( $this->options );
+		if( ! in_array( $sanitized_value, $keys ) ) $sanitized_value = isset( $this->default ) ? $this->default : false;
+		return $sanitized_value;
+	}
+
+}
