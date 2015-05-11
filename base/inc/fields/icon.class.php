ca09db1787c30307589365b69392ab17b7570655
Braam Genis <braamgenis@gmail.com>
2015-05-11 22:11:49 +0200

    Icon field sanitization.

--- base/inc/fields/icon.class.php
+++ base/inc/fields/icon.class.php
@@ -29,6 +29,25 @@ class SiteOrigin_Widget_Field_Icon extends SiteOrigin_Widget_Field {
 	}
 
 	protected function sanitize_field_input( $value ) {
+		$sanitized_value = $value;
+		// Alphanumeric characters and hyphens.
+		if( preg_match( '/[\w\d]+[\w\d-]*/', $sanitized_value, $sanitized_matches ) ) {
+			$sanitized_value = $sanitized_matches[0];
+		}
+		else {
+			$sanitized_value = '';
+		}
+		list( $value_family, $value_icon ) = ( ! empty( $sanitized_value ) && strpos( $sanitized_value, '-' ) !== false ) ? explode( '-', $sanitized_value, 2 ) : array('', '');
+
+		static $widget_icon_families;
+		if( empty( $widget_icon_families ) ) {
+			$widget_icon_families = apply_filters( 'siteorigin_widgets_icon_families', array() );
+		}
+		if( ! ( isset( $widget_icon_families[$value_family] ) && isset( $widget_icon_families[$value_family]['icons'][$value_icon] ) ) ) {
+			$sanitized_value = isset( $this->default ) ? $this->default : '';
+		}
+
+		return $sanitized_value;
 	}
 
 }
