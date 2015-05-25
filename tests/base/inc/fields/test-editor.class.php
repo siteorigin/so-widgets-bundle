5e11e95fab9f0eedda8d810291e4c94cdd261924
Braam Genis <braamgenis@gmail.com>
2015-05-25 15:50:49 +0200

    SiteOrigin_Widget_Field_Slider unit tests

--- tests/base/inc/fields/test-editor.class.php
+++ tests/base/inc/fields/test-editor.class.php
@@ -1,22 +1,30 @@
 <?php
 
 class SiteOrigin_Widget_Field_EditorTest extends WP_UnitTestCase {
-	/**
-	 * @test
-	 */
-	public function render_outputs_correctly() {
+
+	private $field_output;
+
+	function setUp() {
 
 		$field_options = array(
 			'type' => 'editor',
-			'label' => 'An editor input',
+			'label' => 'A editor input',
+			'placeholder' => 'Some placeholder text',
+			'rows' => 10
 		);
 
 		/* @var $base_field SiteOrigin_Widget_Field_Editor */
 		$field = new SiteOrigin_Widget_Field_Editor( 'an_editor_input', 'an_editor_input_id', 'an_editor_input_name', $field_options );
 
-		$actual = get_field_render_output( $field );
+		$this->field_output = get_field_render_output( $field );
+	}
 
-		$this->assertContains( '<textarea type="text" name="an_editor_input_name" id="an_editor_input_id"', $actual);
+	/**
+	 * @test
+	 */
+	public function render_outputs_correctly() {
+
+		$this->assertContains( '<textarea type="text" name="an_editor_input_name" id="an_editor_input_id"', $this->field_output);
 	}
 
 	/**
@@ -24,17 +32,15 @@ class SiteOrigin_Widget_Field_EditorTest extends WP_UnitTestCase {
 	 */
 	public function render_outputs_additional_input_class() {
 
-		$field_options = array(
-			'type' => 'editor',
-			'label' => 'An editor input',
-		);
+		$this->assertContains( 'siteorigin-widget-input-editor', $this->field_output);
+	}
 
-		/* @var $base_field SiteOrigin_Widget_Field_Editor */
-		$field = new SiteOrigin_Widget_Field_Editor( 'an_editor_input', 'an_editor_input_id', 'an_editor_input_name', $field_options );
+	/**
+	 * @test
+	 */
+	public function render_outputs_placeholder_if_present() {
 
-		$actual = get_field_render_output( $field );
-
-		$this->assertContains( 'siteorigin-widget-input-editor', $actual);
+		$this->assertContains( 'placeholder="Some placeholder text"', $this->field_output);
 	}
 
 	/**
@@ -42,17 +48,6 @@ class SiteOrigin_Widget_Field_EditorTest extends WP_UnitTestCase {
 	 */
 	public function render_outputs_rows_attribute() {
 
-		$field_options = array(
-			'type' => 'editor',
-			'label' => 'An editor input',
-			'rows' => 10
-		);
-
-		/* @var $base_field SiteOrigin_Widget_Field_Editor */
-		$field = new SiteOrigin_Widget_Field_Editor( 'an_editor_input', 'an_editor_input_id', 'an_editor_input_name', $field_options );
-
-		$actual = get_field_render_output( $field );
-
-		$this->assertContains( 'rows="10"', $actual);
+		$this->assertContains( 'rows="10"', $this->field_output);
 	}
 }
