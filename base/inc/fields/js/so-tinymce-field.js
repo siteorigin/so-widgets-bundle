6e3b3fab6d988f01529c85f1d4c6dd8781070012
Braam Genis <braamgenis@gmail.com>
2015-06-09 18:32:49 +0200

    Do TinyMCE preinitialization even when not initially active. Always check for and remove quicktags toolbar before initializing quicktags.

--- base/inc/fields/js/so-tinymce-field.js
+++ base/inc/fields/js/so-tinymce-field.js
@@ -15,35 +15,35 @@
                 qtSettings = $.extend({}, tinyMCEPreInit.qtInit['siteorigin-widget-input-tinymce-field'], qtSettings, {id:id});
                 tinyMCEPreInit.qtInit[id] = qtSettings;
                 if( QTags.instances[ id ] != null ) {
-                    container.find('.quicktags-toolbar').remove();
                     delete QTags.instances[ id ];
                 }
+                container.find('.quicktags-toolbar').remove();
                 quicktags(tinyMCEPreInit.qtInit[id]);
                 $textarea.on('input propertychange', function () {
                     container.find('input[type="hidden"]').val($textarea.val());
                 });
+                var mceSettings = sowGetWidgetFieldVariable(formClass, name, 'mceSettings');
+                var fieldName = /[a-zA-Z0-9\-]+\[[a-zA-Z0-9]+\]\[(.*)\]/.exec( name )[1];
+                var idPattern = new RegExp( 'widget-.+-[_a-zA-Z0-9]+-' + fieldName.replace( '][', '-', 'g' ) + '[-\d]*' );
+                for( var initId in tinyMCEPreInit.mceInit) {
+                    if(initId.match(idPattern)) {
+                        mceSettings = $.extend({}, tinyMCEPreInit.mceInit[initId], mceSettings);
+                    }
+                }
+                var setupEditor = function(editor) {
+                    editor.on('change', function() {
+                            tinymce.get(id).save();
+                            $textarea.trigger('input');
+                        }
+                    );
+                };
+                mceSettings = $.extend({}, mceSettings, {selector:'#'+id, setup:setupEditor});
+                tinyMCEPreInit.mceInit[id] = mceSettings;
                 var wrapDiv = container.find('div#wp-' + id + '-wrap');
                 if(wrapDiv.hasClass('tmce-active')) {
                     if(tinymce.get(id) != null) {
                         tinymce.get(id).remove();
                     }
-                    var mceSettings = sowGetWidgetFieldVariable(formClass, name, 'mceSettings');
-                    var fieldName = /[a-zA-Z0-9\-]+\[[a-zA-Z0-9]+\]\[(.*)\]/.exec( name )[1];
-                    var idPattern = new RegExp( 'widget-.+-[_a-zA-Z0-9]+-' + fieldName.replace( '][', '-', 'g' ) + '[-\d]*' );
-                    for( var initId in tinyMCEPreInit.mceInit) {
-                        if(initId.match(idPattern)) {
-                            mceSettings = $.extend({}, tinyMCEPreInit.mceInit[initId], mceSettings);
-                        }
-                    }
-                    var setupEditor = function(editor) {
-                        editor.on('change', function() {
-                                tinymce.get(id).save();
-                                $textarea.trigger('input');
-                            }
-                        );
-                    };
-                    mceSettings = $.extend({}, mceSettings, {selector:'#'+id, setup:setupEditor});
-                    tinyMCEPreInit.mceInit[id] = mceSettings;
 
                     tinymce.init(tinyMCEPreInit.mceInit[id]);
                 }
