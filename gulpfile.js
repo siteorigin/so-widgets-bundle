3b604efa591fd4c6160f8b9688706e8491fd9aab
Braam Genis <braamgenis@gmail.com>
2015-07-01 18:32:15 +0200

    Minification task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
-var closureCompiler = require('gulp-closure-compiler');
+var rename = require('gulp-rename');
 var replace = require('gulp-replace');
+var uglify = require('gulp-uglify');
+//var closureCompiler = require('gulp-closure-compiler');
 
 gulp.task('version', function(version) {
     var args = {};
@@ -21,7 +23,8 @@ gulp.task('version', function(version) {
     return gulp.src('so-widgets-bundle.php')
         .pipe(replace(/(Version: ).*/, '$1'+args.v))
         .pipe(replace(/(define\('SOW_BUNDLE_VERSION', ').*('\);)/, '$1'+args.v+'$2'))
-        .pipe(gulp.dest('build/so-widgets-bundle.php'));
+        .pipe(replace(/(define\('SOW_BUNDLE_JS_SUFFIX', ').*('\);)/, '$1.min$2'))
+        .pipe(gulp.dest('tmp'));
 });
 
 gulp.task('compileLess', function() {
@@ -33,12 +36,21 @@ gulp.task('concatScripts', function () {
 });
 
 gulp.task('minifyScripts', function () {
-    return gulp.src('admin/**/*.js')
-        .pipe(closureCompiler({
-            compilerPath: 'bower_components/closure-compiler/compiler.jar',
-            fileName: 'build.js'
-        }))
-        .pipe(gulp.dest('dist'));
+    return gulp.src(['**/*.js', '!bower_components/**', '!js/**', '!node_modules/**', '!tests/**', '!tmp/**', '!gulpfile.js'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('tmp'));
+
+    //return gulp.src('admin/admin.js')
+    //    .pipe(closureCompiler({
+    //        compilerPath: 'bower_components/closure-compiler/compiler.jar',
+    //        fileName: 'admin.js'
+    //    }))
+    //    .pipe(gulp.dest('tmp'));
 });
 
-gulp.task('release', ['version']);
+gulp.task('compileJS', ['minifyScripts']);
+
+gulp.task('release', ['version', 'compileLess', 'compileJS'], function() {
+
+});
