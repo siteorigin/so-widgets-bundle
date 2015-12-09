738b73c6095286dfdb1b34694ee816129cbeb479
Braam Genis <braamgenis@gmail.com>
2015-12-09 12:10:04 +0200

    so-widgets-bundle-44 Allow LatLong input for Google Maps widget center and markers. Some refactoring to send map configuration options as single data attribute and separate markers and directions functions. Fix for bug when using TinyMCE field in directions waypoints.

--- widgets/google-map/tpl/js-map.php
+++ widgets/google-map/tpl/js-map.php
@@ -1,10 +1,6 @@
 <div
 	class="sow-google-map-canvas"
 	style="height:<?php echo intval($height) ?>px;"
-	id="map-canvas-<?php echo esc_attr($map_id) ?>"
-<?php foreach( $map_data as $key => $val ) : ?>
-	<?php if ( ! empty( $val ) ) : ?>
-	data-<?php echo $key . '="' . esc_attr( $val ) . '"'?>
-	<?php endif ?>
-<?php endforeach; ?>
+	id="map-canvas-<?php echo esc_attr( $map_id ) ?>"
+	data-options="<?php echo esc_attr( json_encode( $map_data ) ) ?>"
 ></div>
