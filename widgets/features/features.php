eb35406851bb8eb7b540afd2b76075fcf983cf41
Braam Genis <braamgenis@gmail.com>
2016-03-15 12:26:32 +0200

    Allow user to select size for uploaded icon image. Ensure icon image styles not repeated in subsequent icon images.

--- widgets/features/features.php
+++ widgets/features/features.php
@@ -8,6 +8,11 @@ Author URI: https://siteorigin.com
 
 class SiteOrigin_Widget_Features_Widget extends SiteOrigin_Widget {
 	function __construct() {
+		$image_size_configs = $this->get_image_sizes();
+		$sizes = array();
+		foreach( $image_size_configs as $name => $size_config) {
+			$sizes[$name] = ucwords(preg_replace('/[-_]/', ' ', $name)) . ' (' . $size_config['width'] . 'x' . $size_config['height'] . ')';
+		}
 		parent::__construct(
 			'sow-features',
 			__( 'SiteOrigin Features', 'so-widgets-bundle' ),
@@ -54,6 +59,12 @@ class SiteOrigin_Widget_Features_Widget extends SiteOrigin_Widget {
 							'library' => 'image',
 							'label' => __('Icon image', 'so-widgets-bundle'),
 							'description' => __('Use your own icon image.', 'so-widgets-bundle'),
+						),
+
+						'icon_image_size' => array(
+							'type' => 'select',
+							'label' => __('Icon image size', 'so-widgets-bundle'),
+							'options' => $sizes,
 						),
 
 						// The text under the icon
@@ -203,6 +214,36 @@ class SiteOrigin_Widget_Features_Widget extends SiteOrigin_Widget {
 			),
 			plugin_dir_path(__FILE__).'../'
 		);
+	}
+
+	/**
+	 * Get size information for all currently-registered image sizes.
+	 * From codex example here: https://codex.wordpress.org/Function_Reference/get_intermediate_image_sizes
+	 *
+	 * @global $_wp_additional_image_sizes
+	 * @uses   get_intermediate_image_sizes()
+	 * @return array $sizes Data for all currently-registered image sizes.
+	 */
+	function get_image_sizes() {
+		global $_wp_additional_image_sizes;
+
+		$sizes = array();
+
+		foreach ( get_intermediate_image_sizes() as $_size ) {
+			if ( in_array( $_size, array('thumbnail', 'medium', 'medium_large', 'large') ) ) {
+				$sizes[ $_size ]['width']  = get_option( "{$_size}_size_w" );
+				$sizes[ $_size ]['height'] = get_option( "{$_size}_size_h" );
+				$sizes[ $_size ]['crop']   = (bool) get_option( "{$_size}_crop" );
+			} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+				$sizes[ $_size ] = array(
+					'width'  => $_wp_additional_image_sizes[ $_size ]['width'],
+					'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+					'crop'   => $_wp_additional_image_sizes[ $_size ]['crop'],
+				);
+			}
+		}
+
+		return $sizes;
 	}
 
 	function initialize() {
