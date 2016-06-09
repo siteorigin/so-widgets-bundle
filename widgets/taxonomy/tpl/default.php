9e13b65e24261a0aa4db47bf868bcbf5ee21d48f
Greg Priday <greg@siteorigin.com>
2016-06-09 18:16:59 +0200

    Properly echo term url

--- widgets/taxonomy/tpl/default.php
+++ widgets/taxonomy/tpl/default.php
@@ -18,7 +18,7 @@
 	<?php endif; ?>
 
 	<?php foreach ( get_the_terms( $post->ID, $taxonomy_name ) as $term ) : ?>
-		<a class="so-taxonomy-<?php echo esc_attr( $display_format )?>" href="<?php get_term_link( $term, $taxonomy_name ) ?>" rel="tag"><?php echo esc_html( $term->name ) ?></a>
+		<a class="so-taxonomy-<?php echo esc_attr( $display_format )?>" href="<?php echo get_term_link( $term, $taxonomy_name ) ?>" rel="tag"><?php echo esc_html( $term->name ) ?></a>
 	<?php endforeach; ?>
 
 </div>
