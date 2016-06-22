f6aec99d956f3f2d6ca98d7784a1b2dee5e382f4
Greg Priday <greg@siteorigin.com>
2016-06-22 09:19:23 +0200

    Added title tag to social links
    Resolves GH-103

--- widgets/social-media-buttons/tpl/default.php
+++ widgets/social-media-buttons/tpl/default.php
@@ -5,13 +5,14 @@
 		$classes[] = "sow-social-media-button-" . sanitize_html_class( $network['name'] );
 		$classes[] = "sow-social-media-button";
 		$button_attributes = array(
-			'class' => esc_attr(implode(' ', $classes))
+			'class' => esc_attr( implode(' ', $classes) ),
+			'title' => sprintf( __( '%s on %s', 'so-widgets-bundle' ), get_bloginfo( 'name' ), ucwords( str_replace( '-', ' ', $network['name'] ) ) ),
 		);
 		if(!empty($instance['design']['new_window'])) $button_attributes['target'] = '_blank';
 		if ( ! empty( $network['url'] ) ) $button_attributes['href'] = sow_esc_url( $network['url'] );
 		?>
 
-		<a <?php foreach($button_attributes as $name => $val) echo $name . '="' . $val . '" ' ?>>
+		<a <?php foreach($button_attributes as $name => $val) echo $name . '="' . esc_attr( $val ) . '" ' ?>>
 			<span>
 				<?php if( !empty( $network['is_custom'] ) ) echo '<!-- premium-' . $network['name'] . ' -->'; ?>
 				<?php echo siteorigin_widget_get_icon( $network['icon_name'] ); ?>
