7e14e7ab0db7e2bac39801613d2ffc53088053cb
Greg Priday <greg@siteorigin.com>
2016-07-17 19:47:30 +0200

    Disable complection and corrections on code field

--- base/inc/fields/code.class.php
+++ base/inc/fields/code.class.php
@@ -15,6 +15,7 @@ class SiteOrigin_Widget_Field_Code extends SiteOrigin_Widget_Field_Text_Input_Ba
 	protected function render_field( $value, $instance ) {
 		?>
 		<textarea type="text" name="<?php echo esc_attr( $this->element_name ) ?>" id="<?php echo esc_attr( $this->element_id ) ?>"
+			autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
 			<?php if ( ! empty( $this->placeholder ) ) echo 'placeholder="' . esc_attr( $this->placeholder ) . '"' ?>
             <?php $this->render_CSS_classes( $this->get_input_classes() ) ?>
                   rows="<?php echo ! empty( $this->rows ) ? intval( $this->rows ) : 4 ?>"
