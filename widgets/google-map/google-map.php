ce654b2b8f89614705bf7ab45cfdd6d51d82010b
Greg Priday <greg@siteorigin.com>
2016-08-02 15:35:15 +0200

    Added required to Maps API key

--- widgets/google-map/google-map.php
+++ widgets/google-map/google-map.php
@@ -63,6 +63,7 @@ class SiteOrigin_Widget_GoogleMap_Widget extends SiteOrigin_Widget {
 					'api_key' => array(
 						'type'        => 'text',
 						'label'       => __( 'API key', 'so-widgets-bundle' ),
+						'required'    => true,
 						'description' => sprintf(
 							__( 'Enter your %sAPI key%s. Your map may not function correctly without one.', 'so-widgets-bundle' ),
 							'<a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank">',
