2acc94ae156b883cbfd38380ef69d475123fdef6
Greg Priday <greg@siteorigin.com>
2016-08-23 15:59:22 +0200

    Fix using_posts_selector

--- base/siteorigin-widget.class.php
+++ base/siteorigin-widget.class.php
@@ -548,13 +548,13 @@ abstract class SiteOrigin_Widget extends WP_Widget {
 	}
 
 	/**
-	 * Checks if the current widget is using a posts selector
+	 * Checks if the current widget is using a posts selector in the main form.
 	 *
 	 * @return bool
 	 */
 	function using_posts_selector(){
 		if( empty( $this->form_options ) ) {
-			$this->form_options = $this->get_widget_form();
+			$this->form_options = $this->form_options();
 		}
 
 		foreach($this->form_options as $field) {
