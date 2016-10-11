b96020a0003940864ed19f83924255884a2e0546
gregpriday <greg@siteorigin.com>
2016-10-11 12:02:16 +0200

    Fixed Icon URL output
    Resolves GH-333

--- widgets/icon/tpl/default.php
+++ widgets/icon/tpl/default.php
@@ -8,7 +8,7 @@
 
 <div class="sow-icon-container sow-icon">
 	<?php if ( ! empty( $url ) ) { ?>
-	<a href="<?php esc_attr_e( $url ) ?>" <?php if ( ! empty( $new_window ) ) echo 'target="_blank"'; ?>>
+	<a href="<?php echo sow_esc_url( $url ) ?>" <?php if ( ! empty( $new_window ) ) echo 'target="_blank"'; ?>>
 	<?php } ?>
 		<?php
 		echo siteorigin_widget_get_icon( $icon );
