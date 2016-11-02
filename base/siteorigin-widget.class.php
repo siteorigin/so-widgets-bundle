535e3206256ffc754fa10ff3fad2997f3c2386d5
Pravdomil <pravdomil.toman@gmail.com>
2016-11-02 22:57:17 +0100

    fix comment

--- base/siteorigin-widget.class.php
+++ base/siteorigin-widget.class.php
@@ -770,7 +770,6 @@ abstract class SiteOrigin_Widget extends WP_Widget {
 			$less = $this->get_less_content( $instance );
 		}
 
-		// Substitute the variables
 		if( !class_exists('SiteOrigin_Widgets_Color_Object') ) require plugin_dir_path( __FILE__ ) . 'inc/color.php';
 
 		// Lets widgets insert their own custom generated LESS
@@ -779,6 +778,7 @@ abstract class SiteOrigin_Widget extends WP_Widget {
 		// Handle less @import statements
 		$less = preg_replace_callback( '/^@import\s+".*?\/?([\w-\.]+)";/m', array( $this, 'get_less_import_contents' ), $less );
 
+		// Substitute the variables
 		$vars = $this->get_less_variables($instance);
 		if( !empty( $vars ) ){
 			foreach($vars as $name => $value) {
