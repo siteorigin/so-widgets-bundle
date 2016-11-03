d9ecb30f75e68049d5e0e7648e1b2629d020373b
Braam Genis <braam@siteorigin.com>
2016-11-03 10:51:16 +0200

    Renamed compat file.

--- so-widgets-bundle.php
+++ so-widgets-bundle.php
@@ -25,7 +25,7 @@ if( !function_exists('siteorigin_widget_get_plugin_path') ) {
 	include plugin_dir_path(__FILE__).'icons/icons.php';
 }
 
-include_once plugin_dir_path(__FILE__).'compat/sow-compat.php';
+include_once plugin_dir_path(__FILE__).'compat/compat.php';
 
 class SiteOrigin_Widgets_Bundle {
 
