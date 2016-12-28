d8f561c47cec88eb8a7bd15fd42addc610bf4ce8
Alex S <alex@siteorigin.com>
2016-12-29 05:00:43 +1000

    Contact Radio: Updated Syntax for readability

--- widgets/contact/fields/radio.class.php
+++ widgets/contact/fields/radio.class.php
@@ -3,17 +3,17 @@
 class SiteOrigin_Widget_ContactForm_Field_Radio extends SiteOrigin_Widget_ContactForm_Field_Base {
 
 	public function render_field( $options ) {
-
-		if ( ! empty( $options['field']['options'] ) ) {
-			?><ul><?php
-			foreach ( $options['field']['options'] as $i => $option ) {
-				?><li>
+		if ( ! empty( $options['field']['options'] ) ): ?>
+		<ul>
+			<?php foreach ( $options['field']['options'] as $i => $option ): ?>
+			<li>
 				<label>
 				<input type="radio" value="<?php echo esc_attr( $option['value'] ) ?>" name="<?php echo esc_attr( $options['field_name'] ) ?>" id="<?php echo esc_attr( $options['field_id'] ) . '-' . $i ?>"<?php echo checked( $option['value'], $options['value'], false ) ?>/>
 				<?php echo esc_html( $option['value'] ); ?>
 				</li>
-			<?php } ?>
-			</ul><?php
-		}
+			<?php endforeach; ?>
+			</ul>
+		<?php
+		endif;
 	}
 }
