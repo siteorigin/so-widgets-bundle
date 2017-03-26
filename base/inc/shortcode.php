a4453e310ad5c6896116888065b364e4b8957c11
gregpriday <greg@siteorigin.com>
2017-03-26 18:47:43 +0200

    Properly work with new Page Builder caching system

--- base/inc/shortcode.php
+++ base/inc/shortcode.php
@@ -12,16 +12,22 @@ function siteorigin_widget_shortcode( $attr, $content = '' ){
 	$attr = shortcode_atts( array(
 		'class' => false,
 		'id' => '',
-	), $attr, 'siteorigin_widget' );
+	), $attr, 'panels_widget' );
+
+	$attr[ 'class' ] = html_entity_decode( $attr[ 'class' ] );
 
 	global $wp_widget_factory;
 	if( ! empty( $attr[ 'class' ] ) && isset( $wp_widget_factory->widgets[ $attr[ 'class' ] ] ) ) {
 		$the_widget = $wp_widget_factory->widgets[ $attr[ 'class' ] ];
 
-		$meta = json_decode( html_entity_decode( $content ), true );
+		// Parse the value of the shortcode
+		preg_match( '/value="(.*?)"/', trim( $content ), $matches );
+		if( ! empty( $matches[1] ) ) {
+			$data = json_decode( html_entity_decode( $matches[1] ), true );
+		}
 
-		$widget_args = ! empty( $meta[ 'args' ] ) ? $meta[ 'args' ] : array();
-		$widget_instance = ! empty( $meta[ 'instance' ] ) ? $meta[ 'instance' ] : array();
+		$widget_args = ! empty( $data[ 'args' ] ) ? $data[ 'args' ] : array();
+		$widget_instance = ! empty( $data[ 'instance' ] ) ? $data[ 'instance' ] : array();
 
 		$widget_args = wp_parse_args( array(
 			'before_widget' => '',
@@ -30,68 +36,36 @@ function siteorigin_widget_shortcode( $attr, $content = '' ){
 			'after_title' => '</h3>',
 		), $widget_args );
 
-		if( is_a( $the_widget, 'SiteOrigin_Widget' ) ) {
-			ob_start();
-			$the_widget->widget( $widget_args, $widget_instance );
-			return ob_get_clean();
-		}
+		ob_start();
+		$the_widget->widget( $widget_args, $widget_instance );
+		return ob_get_clean();
 	}
 }
-add_shortcode( 'siteorigin_widget', 'siteorigin_widget_shortcode' );
+
+function siteorigin_widget_shortcode_register(){
+	global $shortcode_tags;
+
+	// Only load this as a fallback if Page Builder hasn't already registered it.
+	if( empty( $shortcode_tags[ 'siteorigin_widget' ] ) ) {
+		add_shortcode( 'siteorigin_widget', 'siteorigin_widget_shortcode' );
+	}
+}
+add_action( 'plugins_loaded', 'siteorigin_widget_shortcode_register', 15 );
 
 /**
- * Setup the mode where we're rendering for the Page Builder database.
- */
-function siteorigin_widget_shortcode_setup_database_render( $post_id ){
-	delete_post_meta( $post_id, 'siteorigin_widget_instances' );
-	add_action( 'siteorigin_panels_the_widget_html', 'siteorigin_widget_shortcode_for_database_render', 10, 4 );
-}
-add_action( 'siteorigin_panels_setup_database_render', 'siteorigin_widget_shortcode_setup_database_render' );
-
-/**
- * Change the widget HTML for a database render
+ * Tell Page Builder we'll be using the custom siteorigin_widget shortcode
  *
- * @param $html
- * @param $the_widget
- * @param $args
- * @param $instance
+ * @param $shortcode
+ * @param $widget
  *
- * @return mixed
+ * @return string
  */
-function siteorigin_widget_shortcode_for_database_render( $html, $the_widget, $args, $instance ) {
-	if( empty( $GLOBALS[ 'SITEORIGIN_PANELS_DATABASE_RENDER' ] ) ) return $html;
-
-	if( is_a( $the_widget, 'SiteOrigin_Widget_Editor_Widget' ) ) {
-		// The Editor widget handles its own database based rendering
-		return $html;
-	}
-	else if( is_a( $the_widget, 'SiteOrigin_Widget' ) ) {
-		unset( $instance[ 'panels_info' ] );
-
-		$meta = get_post_meta( get_the_ID(), 'siteorigin_widget_instances', true );
-		if( empty( $meta ) ) {
-			$meta = array();
-		}
-		$i = count( $meta );
-
-		$before_widget = $args['before_widget'];
-		$after_widget = $args['after_widget'];
-		unset( $args['before_widget'] );
-		unset( $args['after_widget'] );
-
-		$meta[$i] = array(
-			'args' => $args,
-			'instance' => $instance,
-		);
-		update_post_meta( get_the_ID(), 'siteorigin_widget_instances', $meta );
-
-		$html .= $before_widget;
-		$html .= '[siteorigin_widget ';
-		$html .= 'class="' . get_class( $the_widget ) . '"]';
-		$html .= htmlentities( wp_json_encode( $meta[ $i ] ) );
-		$html .= '[/siteorigin_widget]';
-		$html .= $after_widget;
+function siteorigin_widget_shortcode_panels_cache_shortcode( $shortcode, $widget ){
+	if( is_a( $widget, 'SiteOrigin_Widget' ) ) {
+		$shortcode = 'siteorigin_widget';
 	}
 
-	return $html;
+	return $shortcode;
 }
+
+add_filter( 'siteorigin_panels_cache_shortcode', 'siteorigin_widget_shortcode_panels_cache_shortcode', 10, 2 );
