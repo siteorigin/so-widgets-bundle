5e33b4816db463e39d1159aaadc8ae5f2c9c1606
Braam Genis <braam@siteorigin.com>
2017-04-22 15:16:33 +0200

    Setting values on date-range field.

--- base/inc/fields/js/date-range-field.js
+++ base/inc/fields/js/date-range-field.js
@@ -1,6 +1,28 @@
 (function( $ ) {
 	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-date-range', function ( e ) {
-		var afterPicker = new Pikaday( { field: $( this ).find( '[name="after"]' )[0] } );
-		var beforePicker = new Pikaday( { field: $( this ).find( '[name="before"]' )[0] } );
+
+		var valField = $( this ).find( 'input[type="hidden"][class="siteorigin-widget-input"]' );
+
+		var createPikadayInput = function ( inputName, initVal ) {
+			var $field = $( this ).find( '[name="' + inputName + '"]' );
+			var picker = new Pikaday( {
+				field: $field[0],
+				onSelect: function(date) {
+					var curVal = valField.val() === '' ? {} : JSON.parse( valField.val() );
+					curVal[inputName] = date.toLocaleDateString({}, {year: 'numeric', month:'2-digit', day:'2-digit'})
+					$field.val( curVal[inputName] );
+					valField.val( JSON.stringify( curVal ) );
+				},
+			} );
+
+			if ( initVal ) {
+				$field.val( initVal );
+			}
+			return picker;
+		}.bind( this );
+
+		var initRange = valField.val() === '' ? {after:'', before:''} : JSON.parse( valField.val() )
+		createPikadayInput( 'after', initRange.after );
+		createPikadayInput( 'before', initRange.before );
 	} );
 })( jQuery );
