0d5819386187e652af0ce445b72c495450df7993
Braam Genis <braam@siteorigin.com>
2017-05-23 10:54:44 +0200

    Fixed order field to allow setting of value in front end.

--- base/inc/fields/js/order-field.js
+++ base/inc/fields/js/order-field.js
@@ -3,19 +3,30 @@
     $(document).on( 'sowsetupform', function(e) {
         var $form = $(e.target);
 
-
         $form.find( '.siteorigin-widget-field-type-order' ).each( function(){
             var $$ = $( this );
-
-            $$.find( '.siteorigin-widget-order-items' ).sortable( {
+			var $valField = $$.find( '.siteorigin-widget-input' );
+			var $items = $$.find( '.siteorigin-widget-order-items' );
+			$items.sortable( {
                 stop: function(){
-                    var val = [];
-                    $$.find( '.siteorigin-widget-order-item' ).each( function( i, el ){
-                        val.push( $(el).data('value') );
-                    } );
-                    $$.find('.siteorigin-widget-input').val( val.join(',') );
+					var val = $( this ).sortable( 'toArray', { attribute: 'data-value' } );
+                    $valField.val( val.join(',') );
+					$valField.trigger( 'change', { silent: true } );
                 }
             } );
+
+			$$.change( function ( event, params ) {
+				if ( ! ( params && params.silent ) ) {
+					var values = $valField.val() === '' ? [] : $valField.val().split(',');
+					if ( values.length ) {
+						for ( var i = 0; i < values.length; i++) {
+							var val = values[ i ];
+							var $item = $$.find( '.siteorigin-widget-order-item[data-value=' + val + ']' );
+							$items.append( $item );
+						}
+					}
+				}
+			} );
         } );
     });
 
