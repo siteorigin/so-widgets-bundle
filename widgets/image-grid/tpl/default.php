15c3e37aa3bfae181dbdbf21ee753cc02622a743
Braam Genis <braam@siteorigin.com>
2017-06-20 12:23:38 +0200

    Image grid template code structure a bit more readable.

--- widgets/image-grid/tpl/default.php
+++ widgets/image-grid/tpl/default.php
@@ -12,11 +12,20 @@
 		<?php if( !empty( $max_height ) ) echo 'data-max-height="' . intval( $max_height ) . '"' ?>>
 		<?php foreach( $images as $image ) : ?>
 			<div class="sow-image-grid-image">
-			<?php if ( ! empty( $image['url'] ) ) : ?><a href="<?php echo sow_esc_url( $image['url'] ) ?>" <?php foreach( $image['link_attributes'] as $att => $val ) if ( ! empty( $val ) ) : echo $att.'="' . esc_attr( $val ) . '" '; endif; ?>><?php endif; ?>
-			<?php echo wp_get_attachment_image( $image['image'], $attachment_size, false, array(
-				'title' => $image['title']
-			) );?>
-			<?php if ( ! empty( $image['url'] ) ) : ?></a><?php endif; ?>
+				<?php if ( ! empty( $image['url'] ) ) : ?>
+					<a href="<?php echo sow_esc_url( $image['url'] ) ?>"
+					<?php foreach( $image['link_attributes'] as $att => $val ) : ?>
+						<?php if ( ! empty( $val ) ) : ?>
+							<?php echo $att.'="' . esc_attr( $val ) . '" '; ?>
+						<?php endif; ?>
+					<?php endforeach; ?>>
+				<?php endif; ?>
+				<?php echo wp_get_attachment_image( $image['image'], $attachment_size, false, array(
+					'title' => $image['title']
+				) );?>
+				<?php if ( ! empty( $image['url'] ) ) : ?>
+					</a>
+				<?php endif; ?>
 			</div>
 		<?php endforeach; ?>
 	</div>
