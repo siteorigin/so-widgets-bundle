7b729910657c5755e6315dfab5a88bb682a2fea4
Braam Genis <braam@siteorigin.com>
2017-07-06 17:22:28 +0200

    Use editor settings from back end. Configure editor to use all buttons.

--- base/inc/fields/editor.class.php
+++ base/inc/fields/editor.class.php
@@ -10,13 +10,28 @@ class SiteOrigin_Widget_Field_Editor extends SiteOrigin_Widget_Field_Text_Input_
 
 
 	protected function render_field( $value, $instance ) {
-		?>
-		<textarea id="<?php echo esc_attr( $this->element_id ) ?>"
-		          name="<?php echo esc_attr( $this->element_name ) ?>"
-			<?php $this->render_data_attributes( $this->get_input_data_attributes() ) ?>
-			<?php $this->render_CSS_classes( $this->get_input_classes() ) ?>
-			<?php if ( ! empty( $this->placeholder ) ) echo 'placeholder="' . esc_attr( $this->placeholder ) . '"' ?>
-			<?php if( ! empty( $this->readonly ) ) echo 'readonly' ?>></textarea>
+		$settings = array(
+			'tinymce' => array(
+				'wp_skip_init' => strpos( $this->element_id, '__i__' ) != false ||
+				                  strpos( $this->element_id, '_id_' ) != false,
+				'toolbar1' => 'formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_more,wp_adv',
+				'toolbar2' => 'strikethrough,hr,forecolor,pastetext,removeformat,charmap,outdent,indent,undo,redo,wp_help',
+				'wpautop' => true,
+			),
+			'quicktags' => array(
+				'buttons' => 'strong,em,link,block,del,ins,img,ul,ol,li,code,more,close',
+			),
+		);
+		
+		?><div class="siteorigin-widget-tinymce-container"
+		       data-editor-settings="<?php echo esc_attr( json_encode( $settings ) ) ?>">
+			<textarea id="<?php echo esc_attr( $this->element_id ) ?>"
+			          name="<?php echo esc_attr( $this->element_name ) ?>"
+				<?php $this->render_data_attributes( $this->get_input_data_attributes() ) ?>
+				<?php $this->render_CSS_classes( $this->get_input_classes() ) ?>
+				<?php if ( ! empty( $this->placeholder ) ) echo 'placeholder="' . esc_attr( $this->placeholder ) . '"' ?>
+				<?php if( ! empty( $this->readonly ) ) echo 'readonly' ?>></textarea>
+		</div>
 		<?php
 
 	}
--- base/inc/fields/js/editor-field.js
+++ base/inc/fields/js/editor-field.js
@@ -3,15 +3,12 @@
 (function( $ ) {
 	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-editor', function ( e ) {
 		var $$ = $(this);
-		var $textarea = $$.find( '> textarea' );
+		var $container = $$.find( '.siteorigin-widget-tinymce-container' );
+		var settings = $container.data( 'editorSettings' );
+		var $textarea = $container.find( '> textarea' );
 		var id = $textarea.attr( 'id' );
 
-		wp.editor.initialize( id, {
-			tinymce: {
-				wpautop: true
-			},
-			quicktags: true
-		} );
+		wp.editor.initialize( id, settings );
 
 		editor = window.tinymce.get( id );
 	});
