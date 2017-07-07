89e0f0ae50124dfc200ee25db96281c336f54b35
Braam Genis <braam@siteorigin.com>
2017-07-07 16:58:16 +0200

    Allow setting default editor and remember editor state.

--- base/inc/fields/editor.class.php
+++ base/inc/fields/editor.class.php
@@ -15,16 +15,51 @@ class SiteOrigin_Widget_Field_Editor extends SiteOrigin_Widget_Field_Text_Input_
 	 * @var int
 	 */
 	protected $editor_height;
+	/**
+	 * The editor to be displayed initially.
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $default_editor = 'tinymce';
+	/**
+	 * The last editor selected by the user.
+	 *
+	 * @access protected
+	 * @var string
+	 */
+	protected $selected_editor;
 
 	protected function initialize() {
 		if ( ! is_admin() ) {
 			return;
 		}
 	}
-
-
+	
+	protected function get_input_classes() {
+		$classes = parent::get_input_classes();
+		$classes[] = 'wp-editor-area';
+		return $classes;
+	}
+	
+	
+	protected function render_before_field( $value, $instance ) {
+		$selected_editor_name = $this->get_selected_editor_field_name( $this->base_name );
+		if( ! empty( $instance[ $selected_editor_name ] ) ) {
+			$this->selected_editor = $instance[ $selected_editor_name ];
+		}
+		else {
+			$this->selected_editor = $this->default_editor;
+		}
+		parent::render_before_field( $value, $instance );
+	}
+	
 	protected function render_field( $value, $instance ) {
+		
+		$selected_editor = in_array( $this->selected_editor, array( 'tinymce', 'tmce' ) ) ? 'tmce' : 'html';
+		
 		$settings = array(
+			'selectedEditor' => $selected_editor,
 			'tinymce' => array(
 				'wp_skip_init' => strpos( $this->element_id, '__i__' ) != false ||
 				                  strpos( $this->element_id, '_id_' ) != false,
@@ -36,6 +71,12 @@ class SiteOrigin_Widget_Field_Editor extends SiteOrigin_Widget_Field_Text_Input_
 				'buttons' => 'strong,em,link,block,del,ins,img,ul,ol,li,code,more,close',
 			),
 		);
+		
+		$value = apply_filters( 'the_editor_content', $value, $this->selected_editor );
+		
+		if ( false !== stripos( $value, 'textarea' ) ) {
+			$value = preg_replace( '%</textarea%i', '&lt;/textarea', $value );
+		}
 		
 		?><div class="siteorigin-widget-tinymce-container"
 		       data-editor-settings="<?php echo esc_attr( json_encode( $settings ) ) ?>">
@@ -49,8 +90,12 @@ class SiteOrigin_Widget_Field_Editor extends SiteOrigin_Widget_Field_Text_Input_
 				<?php $this->render_data_attributes( $this->get_input_data_attributes() ) ?>
 				<?php $this->render_CSS_classes( $this->get_input_classes() ) ?>
 				<?php if ( ! empty( $this->placeholder ) ) echo 'placeholder="' . esc_attr( $this->placeholder ) . '"' ?>
-				<?php if( ! empty( $this->readonly ) ) echo 'readonly' ?>></textarea>
+				<?php if( ! empty( $this->readonly ) ) echo 'readonly' ?>><?php echo $value ?></textarea>
 		</div>
+		<input type="hidden"
+		       name="<?php echo esc_attr( $this->for_widget->so_get_field_name( $this->base_name . '_selected_editor', $this->parent_container) ) ?>"
+		       class="siteorigin-widget-input siteorigin-widget-tinymce-selected-editor"
+		       value="<?php echo esc_attr( $this->selected_editor ) ?>"/>
 		<?php
 
 	}
@@ -65,6 +110,20 @@ class SiteOrigin_Widget_Field_Editor extends SiteOrigin_Widget_Field_Text_Input_
 	}
 
 	public function sanitize_instance( $instance ) {
+		$selected_editor_name = $this->get_selected_editor_field_name( $this->base_name );
+		if( ! empty( $instance[ $selected_editor_name ] ) ) {
+			$selected_editor = $instance[ $selected_editor_name ];
+			$instance[ $selected_editor_name ] = in_array( $selected_editor, array( 'tinymce', 'tmce', 'quicktags', 'html' ) ) ? $selected_editor : $this->default_editor;
+		}
 		return $instance;
+	}
+	
+	public function get_selected_editor_field_name( $base_name ) {
+		$v_name = $base_name;
+		if( strpos($v_name, '][') !== false ) {
+			// Remove this splitter
+			$v_name = substr( $v_name, strrpos($v_name, '][') + 2 );
+		}
+		return $v_name . '_selected_editor';
 	}
 }
--- base/inc/fields/js/editor-field.js
+++ base/inc/fields/js/editor-field.js
@@ -8,9 +8,39 @@
 		var $textarea = $container.find( '> textarea' );
 		var id = $textarea.attr( 'id' );
 
+		$( document ).on( 'tinymce-editor-setup', function () {
+
+			if ( ! $$.find( '.wp-editor-wrap' ).hasClass( settings.selectedEditor + '-active' ) ) {
+				setTimeout( function () {
+					window.switchEditors.go( id );
+				}, 10 );
+			}
+		} );
 		wp.editor.initialize( id, settings );
 
-		editor = window.tinymce.get( id );
+		$$.on( 'click', function( event ) {
+
+			var $target = $( event.target );
+			if ( $target.hasClass( 'wp-switch-editor' ) ) {
+				var mode = $target.hasClass( 'switch-tmce' ) ? 'tmce' : 'html';
+				if ( mode === 'tmce' ) {
+					// Quick bit of sanitization to prevent catastrophic backtracking in TinyMCE HTML parser regex
+					var editor = tinymce.get( id );
+					if (editor !== null) {
+						var content = $textarea.val();
+						if (content.search( '<' ) !== -1) {
+							if (content.search( '>' ) === -1) {
+								content = content.replace( /</g, '' );
+								$textarea.val( content );
+							}
+						}
+						// editor.setContent(window.switchEditors.wpautop(content));
+					}
+				}
+
+				$$.find( '.siteorigin-widget-tinymce-selected-editor' ).val( mode );
+			}
+		});
 	});
 
 })( jQuery );
