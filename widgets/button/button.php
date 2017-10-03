bb49068392625f9dede67fd13173f92419596538
Braam Genis <braam@siteorigin.com>
2017-10-03 16:58:04 +0200

    To fix #564. Button: Use `esc_js` instead of `esc_attr` for onclick.

--- widgets/button/button.php
+++ widgets/button/button.php
@@ -250,9 +250,6 @@ class SiteOrigin_Widget_Button_Widget extends SiteOrigin_Widget {
 		if ( ! empty( $attributes['title'] ) ) {
 			$button_attributes['title'] = $attributes['title'];
 		}
-		if ( ! empty( $attributes['onclick'] ) ) {
-			$button_attributes['onclick'] = $attributes['onclick'];
-		}
 		if ( ! empty( $attributes['rel'] ) ) {
 			$button_attributes['rel'] = $attributes['rel'];
 		}
@@ -269,6 +266,7 @@ class SiteOrigin_Widget_Button_Widget extends SiteOrigin_Widget {
 		return array(
 			'button_attributes' => $button_attributes,
 			'href' => !empty( $instance['url'] ) ? $instance['url'] : '#',
+			'onclick' => ! empty( $attributes['onclick'] ) ? $attributes['onclick'] : '',
 			'align' => $instance['design']['align'],
 			'icon_image_url' => $icon_image_url,
 			'icon' => $instance['button_icon']['icon_selected'],
