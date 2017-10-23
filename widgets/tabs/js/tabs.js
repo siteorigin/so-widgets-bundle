27adcb0d742a6e1bf8288a31c8e2f3d6ae405363
Braam Genis <braam@siteorigin.com>
2017-10-23 16:59:42 +0200

    Tabs: Resize tab panel container height when switching tabs.

--- widgets/tabs/js/tabs.js
+++ widgets/tabs/js/tabs.js
@@ -3,12 +3,18 @@ jQuery( function ( $ ) {
 	$( '.sow-tabs' ).each( function ( index, element ) {
 		var $this = $( element );
 		
+		var $tabPanelsContainer = $this.find( '> .sow-tabs-panel-container' );
+		
 		var $tabs = $this.find( '> .sow-tabs-tab-container > .sow-tabs-tab' );
 		
 		var selectedIndex = $this.find( '.sow-tabs-tab-selected' ).index();
 		
-		var $tabPanels = $this.find( '> .sow-tabs-panel-container > .sow-tabs-panel' );
+		var $tabPanels = $tabPanelsContainer.find( '> .sow-tabs-panel' );
 		$tabPanels.not(':eq(' + selectedIndex + ')').hide();
+		
+		setTimeout( function () {
+			$tabPanelsContainer.height( $tabPanels.eq( selectedIndex ).outerHeight() );
+		}, 100 );
 		
 		$tabs.click( function () {
 			var $this = $( this );
@@ -20,6 +26,7 @@ jQuery( function ( $ ) {
 			$tabPanels.not( ':eq(' + selectedIndex + ')' ).fadeOut( 'fast' );
 			$this.addClass( 'sow-tabs-tab-selected' );
 			$tabPanels.eq( selectedIndex ).fadeIn( 'fast' );
+			$tabPanelsContainer.height( $tabPanels.eq( selectedIndex ).outerHeight() );
 		} );
 	} );
 } );
