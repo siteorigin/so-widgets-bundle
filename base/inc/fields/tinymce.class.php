5dcbbcae17c14763b94cf050cde946ad31e8b280
Alex S <AlexGStapleton@users.noreply.github.com>
2018-01-03 01:16:11 +1000

    Editor: stop removing grunion_media_button on widgets page

--- base/inc/fields/tinymce.class.php
+++ base/inc/fields/tinymce.class.php
@@ -516,16 +516,7 @@ class SiteOrigin_Widget_Field_TinyMCE extends SiteOrigin_Widget_Field_Text_Input
 		
 		echo '<div id="wp-' . esc_attr( $editor_id ) . '-media-buttons" class="wp-media-buttons">';
 		
-		$screen = function_exists( 'get_current_screen' ) ? get_current_screen() : null;
-		// Temporarily disable the Jetpack Grunion contact form editor on the widgets screen.
-		if( ! is_null( $screen ) && $screen->id == 'widgets' ) {
-			remove_action( 'media_buttons', 'grunion_media_button', 999 );
-		}
 		do_action( 'media_buttons', $editor_id );
-		// Temporarily disable the Jetpack Grunion contact form editor on the widgets screen.
-		if( ! is_null( $screen ) && $screen->id == 'widgets' ) {
-			add_action( 'media_buttons', 'grunion_media_button', 999 );
-		}
 		
 		echo "</div>\n";
 		
