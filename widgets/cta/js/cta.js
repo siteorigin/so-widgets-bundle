2f2ae3a89919f120827be137fa7c4806d00d3c5a
Braam Genis <braam@siteorigin.com>
2018-07-13 11:48:46 +0200

    Initialize CTA, Price Table and Video JS widgets correctly when in accordion/tabs widgets.

--- widgets/cta/js/cta.js
+++ widgets/cta/js/cta.js
@@ -1,12 +1,21 @@
+/* globals jQuery, sowb */
+
+var sowb = window.sowb || {};
+
 jQuery( function ( $ ) {
-	$( '.sow-cta-base' ).each( function () {
-		var $$ = $( this );
-		var
-			$b = $$.find( '.so-widget-sow-button' ),
-			$t = $$.find( '.sow-cta-text' );
-		
-		if ( $t.outerHeight() > $b.outerHeight() ) {
-			$b.css( 'margin-top', ( $t.outerHeight() - $b.outerHeight() ) / 2 );
-		}
-	} );
+	var setupCta = function () {
+		$( '.sow-cta-base' ).each( function () {
+			var $$ = $( this );
+			var
+				$b = $$.find( '.so-widget-sow-button' ),
+				$t = $$.find( '.sow-cta-text' );
+			
+			if ( $t.outerHeight() > $b.outerHeight() ) {
+				$b.css( 'margin-top', ( $t.outerHeight() - $b.outerHeight() ) / 2 );
+			}
+		} );
+	};
+	setupCta();
+	
+	$( sowb ).on( 'setup_widgets', setupCta );
 } );
--- widgets/price-table/js/pricetable.js
+++ widgets/price-table/js/pricetable.js
@@ -1,46 +1,66 @@
+/* globals jQuery, sowb */
+
+var sowb = window.sowb || {};
+
 jQuery( function ( $ ) {
-	$( '.so-widget-sow-price-table .sow-equalize-row-heights' ).each( function () {
-		var $pt = $( this );
-		var equalizeHeights = function ( selector ) {
-			var maxHeight = 0;
-			var $elements = $pt.find( selector );
-			$elements.each( function () {
-				maxHeight = Math.max( maxHeight, $( this ).height() );
+	var setupPriceTable = function () {
+		$( '.so-widget-sow-price-table' ).each( function () {
+			var $priceTable = $( this );
+			
+			if ( ! $priceTable.is( ':visible' ) || $priceTable.data( 'initialized' ) ) {
+				return $priceTable;
+			}
+			
+			$priceTable.find( '.sow-equalize-row-heights' ).each( function () {
+				var $pt = $( this );
+				var equalizeHeights = function ( selector ) {
+					var maxHeight = 0;
+					var $elements = $pt.find( selector );
+					$elements.each( function () {
+						maxHeight = Math.max( maxHeight, $( this ).height() );
+					} );
+					$elements.height( maxHeight );
+				};
+				
+				var maxFeatures = 0;
+				$pt.find( '.ow-pt-features' ).each( function () {
+					maxFeatures = Math.max( maxFeatures, $( this ).find( '.ow-pt-feature' ).length );
+				} );
+				
+				for ( var i = 0; i < maxFeatures; i++ ) {
+					equalizeHeights( '.ow-pt-feature-index-' + i );
+				}
+				
+				var selectors = [ '.ow-pt-title', '.ow-pt-details', '.ow-pt-image', '.ow-pt-features', '.ow-pt-button' ];
+				selectors.forEach( equalizeHeights );
 			} );
-			$elements.height( maxHeight );
-		};
-		
-		var maxFeatures = 0;
-		$pt.find( '.ow-pt-features' ).each( function () {
-			maxFeatures = Math.max( maxFeatures, $( this ).find( '.ow-pt-feature' ).length );
+			
+			$( '.ow-pt-icon[data-icon]' ).each( function () {
+				var $$ = $( this );
+				var icon = $$.data( 'icon' );
+				
+				if ( $( '#so-pt-icon-' + icon ).length ) {
+					var svg = $( '#so-pt-icon-' + icon + ' svg' ).clone().css( {
+						'max-width': 24,
+						'max-height': 24
+					} );
+					
+					if ( $$.data( 'icon-color' ) !== '' ) {
+						svg.find( 'path' ).css( 'fill', $$.data( 'icon-color' ) );
+					}
+					else {
+						svg.find( 'path' ).css( 'fill', '#333333' );
+					}
+					
+					$$.append( svg );
+				}
+			} );
+			
+			$priceTable.data( 'initialized', true );
 		} );
-		
-		for ( var i = 0; i < maxFeatures; i++ ) {
-			equalizeHeights( '.ow-pt-feature-index-' + i );
-		}
-		
-		var selectors = [ '.ow-pt-title', '.ow-pt-details', '.ow-pt-image', '.ow-pt-features', '.ow-pt-button' ];
-		selectors.forEach( equalizeHeights );
-	} );
+	};
 	
-	$( '.ow-pt-icon[data-icon]' ).each( function () {
-		var $$ = $( this );
-		var icon = $$.data( 'icon' );
-		
-		if ( $( '#so-pt-icon-' + icon ).length ) {
-			var svg = $( '#so-pt-icon-' + icon + ' svg' ).clone().css( {
-				'max-width': 24,
-				'max-height': 24
-			} );
-			
-			if ( $$.data( 'icon-color' ) !== '' ) {
-				svg.find( 'path' ).css( 'fill', $$.data( 'icon-color' ) );
-			}
-			else {
-				svg.find( 'path' ).css( 'fill', '#333333' );
-			}
-			
-			$$.append( svg );
-		}
-	} )
+	setupPriceTable();
+	
+	$( sowb ).on( 'setup_widgets', setupPriceTable );
 } );
