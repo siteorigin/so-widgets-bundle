8c15a5c3a9731f9650df95c9953826a099ff05bc
Braam Genis <braam@siteorigin.com>
2018-07-24 17:30:57 +0200

    Readded special handling for TinyMCE fields to fix `p`s disappearing when previewing in Gutenberg. lol

--- base/js/admin.js
+++ base/js/admin.js
@@ -993,6 +993,19 @@ var sowbForms = window.sowbForms || {};
 					} else {
 						return;
 					}
+				} else if ( $$.prop( 'tagName' ) === 'TEXTAREA' && $$.hasClass( 'wp-editor-area' ) ) {
+					// This is a TinyMCE editor, so we'll use the tinyMCE object to get the content
+					var editor = null;
+					if ( typeof tinyMCE !== 'undefined' ) {
+						editor = tinyMCE.get( $$.attr( 'id' ) );
+					}
+					
+					if ( editor !== null && typeof( editor.getContent ) === "function" && !editor.isHidden() ) {
+						fieldValue = editor.getContent();
+					}
+					else {
+						fieldValue = $$.val();
+					}
 				} else if ( $$.prop( 'tagName' ) === 'SELECT' ) {
 					var selected = $$.find( 'option:selected' );
 					if ( selected.length === 1 ) {
