40868f2beca3f21184ad8cb90266fdb4aff4612c
Braam Genis <braam@siteorigin.com>
2018-12-05 17:28:59 -0800

    Widget block: Remove unnecessary and duplicate API requests.

--- compat/block-editor/widget-block.js
+++ compat/block-editor/widget-block.js
@@ -41,31 +41,13 @@
 		},
 		
 		edit: withState( {
-			loadingWidgets: true,
 			editing: false,
 			formInitialized: false,
 			previewInitialized: false,
-			widgets: null,
 			widgetFormHtml: '',
 			widgetSettingsChanged: false,
 			widgetPreviewHtml: '',
 		} )( function ( props ) {
-			
-			if ( props.loadingWidgets ) {
-				$.get( {
-					url: sowbBlockEditorAdmin.restUrl + 'sowb/v1/widgets',
-					beforeSend: function ( xhr ) {
-						xhr.setRequestHeader( 'X-WP-Nonce', sowbBlockEditorAdmin.nonce );
-					}
-				} )
-				.then( function( widgets ) {
-					var newState = { widgets: widgets, loadingWidgets: false };
-					if ( !props.attributes.widgetClass ) {
-						newState.editing = true;
-					}
-					props.setState( newState );
-				} );
-			}
 			
 			function onWidgetClassChange( newWidgetClass ) {
 				if ( newWidgetClass !== '' ) {
@@ -74,6 +56,7 @@
 					}
 					props.setAttributes( { widgetClass: newWidgetClass, widgetData: null } );
 					props.setState( {
+						editing: true,
 						widgetFormHtml: null,
 						formInitialized: false,
 						widgetSettingsChanged: false,
@@ -127,10 +110,10 @@
 				}
 			}
 			
-			if ( props.editing ) {
+			if ( props.editing || ! props.attributes.widgetClass ) {
 				var widgetsOptions = [];
-				if ( props.widgets ) {
-					props.widgets.sort( function ( a, b ) {
+				if ( sowbBlockEditorAdmin.widgets ) {
+					sowbBlockEditorAdmin.widgets.sort( function ( a, b ) {
 						if ( a.name < b.name ) {
 							return -1;
 						} else if ( a.name > b.name ) {
@@ -138,14 +121,14 @@
 						}
 						return 0;
 					} );
-					widgetsOptions = props.widgets.map( function ( widget ) {
+					widgetsOptions = sowbBlockEditorAdmin.widgets.map( function ( widget ) {
 						return { value: widget.class, label: widget.name };
 					} );
 					widgetsOptions.unshift( { value: '', label: __( 'Select widget type', 'so-widgets-bundle' ) } );
 				}
 				
-				var loadingWidgetForm = props.attributes.widgetClass && !props.widgetFormHtml;
-				if ( loadingWidgetForm ) {
+				var loadWidgetForm = props.attributes.widgetClass && ! props.widgetFormHtml;
+				if ( loadWidgetForm ) {
 					$.post( {
 						url: sowbBlockEditorAdmin.restUrl + 'sowb/v1/widgets/forms',
 						beforeSend: function ( xhr ) {
@@ -188,7 +171,7 @@
 							label: __( 'SiteOrigin Widget', 'so-widgets-bundle' ),
 							instructions: __( 'Select the type of widget you want to use:', 'so-widgets-bundle' )
 						},
-						( props.loadingWidgets || loadingWidgetForm ?
+						( props.loadingWidgets || loadWidgetForm ?
 							el( Spinner ) :
 							el(
 								'div',
@@ -212,8 +195,8 @@
 				];
 			} else {
 				
-				var loadingWidgetPreview = !props.loadingWidgets && !props.editing && !props.widgetPreviewHtml;
-				if ( loadingWidgetPreview ) {
+				var loadWidgetPreview = ! props.loadingWidgets && ! props.editing && ! props.widgetPreviewHtml && props.attributes.widgetClass;
+				if ( loadWidgetPreview ) {
 					$.post( {
 						url: sowbBlockEditorAdmin.restUrl + 'sowb/v1/widgets/previews',
 						beforeSend: function ( xhr ) {
@@ -256,7 +239,7 @@
 							key: 'preview',
 							className: 'so-widget-preview-container'
 						},
-						( loadingWidgetPreview ?
+						( loadWidgetPreview ?
 							el( 'div', {
 									className: 'so-widgets-spinner-container'
 								},
