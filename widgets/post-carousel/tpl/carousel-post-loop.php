90f163cacda856cdc512c7742af8cd05e7fb9187
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2019-02-12 05:20:32 +1000

    Post Carousel: Label navigation and carousel items
    Resolves issues where accessability tools will flag these as non-descriptive links.

--- widgets/post-carousel/tpl/carousel-post-loop.php
+++ widgets/post-carousel/tpl/carousel-post-loop.php
@@ -7,15 +7,15 @@ while($posts->have_posts()) : $posts->the_post(); ?>
 	<li class="sow-carousel-item<?php if( is_rtl() ) echo ' rtl' ?>">
 		<div class="sow-carousel-thumbnail">
 			<?php if( has_post_thumbnail() ) : $img = wp_get_attachment_image_src(get_post_thumbnail_id(), $instance['image_size']); ?>
-				<a href="<?php the_permalink() ?>" style="background-image: url(<?php echo sow_esc_url($img[0]) ?>)">
+				<a href="<?php the_permalink() ?>" style="background-image: url(<?php echo sow_esc_url($img[0]) ?>)" aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>">
 					<span class="overlay"></span>
 				</a>
 			<?php else : ?>
 				<a href="<?php the_permalink() ?>" class="sow-carousel-default-thumbnail"
 				<?php echo ! empty( $default_thumbnail ) ?
-				'style="background-image: url('. sow_esc_url( $default_thumbnail ) .')"' : '' ?>><span class="overlay"></span></a>
+				'style="background-image: url('. sow_esc_url( $default_thumbnail ) .')"' : '' ?> aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>"><span class="overlay"></span></a>
 			<?php endif; ?>
 		</div>
-		<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+		<h3><a href="<?php the_permalink() ?>" id="sow-carousel-id-<?php echo the_ID(); ?>"><?php the_title() ?></a></h3>
 	</li>
 <?php endwhile; wp_reset_postdata(); ?>
