cd27f6df266bbc437fcee3eaa3c4552b9b25520e
Andrew Misplon <amisplon@gmail.com>
2019-10-21 20:49:44 +0200

    Aligned Hero labels to standard capitalization

--- widgets/hero/hero.php
+++ widgets/hero/hero.php
@@ -232,7 +232,7 @@ class SiteOrigin_Widget_Hero_Widget extends SiteOrigin_Widget_Base_Slider {
 
 					'fittext_compressor' => array(
 						'type' => 'number',
-						'label' => __( 'FitText Compressor Strength', 'so-widgets-bundle' ),
+						'label' => __( 'FitText compressor strength', 'so-widgets-bundle' ),
 						'description' => __( 'The higher the value, the more your headings will be scaled down. Values above 1 are allowed.', 'so-widgets-bundle' ),
 						'default' => 0.85,
 						'state_handler' => array(
@@ -280,7 +280,7 @@ class SiteOrigin_Widget_Hero_Widget extends SiteOrigin_Widget_Base_Slider {
 
 					'link_color_hover' => array(
 						'type' => 'color',
-						'label' => __( 'Link Hover Color', 'so-widgets-bundle' )
+						'label' => __( 'Link hover color', 'so-widgets-bundle' )
 					),
 
 				)
