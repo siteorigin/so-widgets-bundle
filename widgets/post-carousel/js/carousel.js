72d0e719e0d6c51898760f66b646945b04f0b33f
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-04-13 20:57:46 +1000

    Remove swipe/touch related JS

--- widgets/post-carousel/js/carousel.js
+++ widgets/post-carousel/js/carousel.js
@@ -8,7 +8,6 @@ jQuery( function ( $ ) {
 		// The carousel widget
 		$( '.sow-carousel-wrapper' ).each( function () {
 
-			var $$ = $( this ),
 				$postsContainer = $$.closest( '.sow-carousel-container' ),
 				$container = $$.closest( '.sow-carousel-container' ).parent(),
 				$itemsContainer = $$.find( '.sow-carousel-items' ),
@@ -86,100 +85,6 @@ jQuery( function ( $ ) {
 				}
 			);
 
-			// Verify "swipe" method exists prior to invoking it.
-			if ( 'function' === typeof $$.swipe ) {
-				var validSwipe = false;
-				var prevDistance = 0;
-				var startPosition = 0;
-				var velocity = 0;
-				var prevTime = 0;
-				var posInterval;
-				var negativeDirection = isRTL ? 'right' : 'left';
-
-				var setNewPosition = function ( newPosition ) {
-					if ( newPosition < 50 && newPosition > -( itemWidth * numItems ) ) {
-						$itemsContainer.css( 'transition-duration', "0s" );
-						$itemsContainer.css( updateProp, newPosition + 'px' );
-						return true;
-					}
-					return false;
-				};
-
-				var setFinalPosition = function () {
-					var finalPosition = parseInt( $itemsContainer.css( updateProp ) );
-					position = Math.abs( Math.round( finalPosition / itemWidth ) );
-					updatePosition();
-				};
-
-				$$.on( 'click', '.sow-carousel-item a',
-					function ( event ) {
-						if ( validSwipe ) {
-							event.preventDefault();
-							validSwipe = false;
-						}
-					}
-				);
-
-				$$.swipe( {
-					excludedElements: "",
-					triggerOnTouchEnd: true,
-					threshold: 75,
-					swipeStatus: function ( event, phase, direction, distance, duration, fingerCount, fingerData ) {
-						if ( direction === 'up' || direction === 'down' ) {
-							return false;
-						}
-
-						if ( phase === "start" ) {
-							startPosition = -( itemWidth * position);
-							prevTime = new Date().getTime();
-							clearInterval( posInterval );
-						}
-						else if ( phase === "move" ) {
-							if ( direction === negativeDirection ) {
-								distance *= -1;
-							}
-							setNewPosition( startPosition + distance );
-							var newTime = new Date().getTime();
-							var timeDelta = (newTime - prevTime) / 1000;
-							velocity = (distance - prevDistance) / timeDelta;
-							prevTime = newTime;
-							prevDistance = distance;
-						}
-						else if ( phase === "end" ) {
-							validSwipe = true;
-							if ( direction === negativeDirection ) {
-								distance *= -1;
-							}
-							if ( Math.abs( velocity ) > 400 ) {
-								velocity *= 0.1;
-								var startTime = new Date().getTime();
-								var cumulativeDistance = 0;
-								posInterval = setInterval( function () {
-									var time = (new Date().getTime() - startTime) / 1000;
-									cumulativeDistance += velocity * time;
-									var newPos = startPosition + distance + cumulativeDistance;
-									var decel = 30;
-									var end = (Math.abs( velocity ) - decel) < 0;
-									if ( direction === negativeDirection ) {
-										velocity += decel;
-									} else {
-										velocity -= decel;
-									}
-									if ( end || !setNewPosition( newPos ) ) {
-										clearInterval( posInterval );
-										setFinalPosition();
-									}
-								}, 20 );
-							} else {
-								setFinalPosition();
-							}
-						}
-						else if ( phase === "cancel" ) {
-							updatePosition();
-						}
-					}
-				} );
-			}
 		} );
 	};
 
