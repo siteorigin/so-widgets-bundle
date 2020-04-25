31ede3a12e7d78ec47b4057e5182330b958c4615
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-04-25 22:59:51 +1000

    Revert "Post Carousel: Remove Blue Hover Effect"
    This reverts commit f3d149ba47240461901271da779602df8b2328ae.

--- widgets/post-carousel/tpl/carousel-post-loop.php
+++ widgets/post-carousel/tpl/carousel-post-loop.php
@@ -4,15 +4,16 @@
  * @var string $default_thumbnail
  */
 while($posts->have_posts()) : $posts->the_post(); ?>
-	<div class="sow-carousel-item">
+	<div class="sow-carousel-item<?php if( is_rtl() ) echo ' rtl' ?>">
 		<div class="sow-carousel-thumbnail">
 			<?php if( has_post_thumbnail() ) : $img = wp_get_attachment_image_src(get_post_thumbnail_id(), $instance['image_size']); ?>
-				<a href="<?php the_permalink() ?>" style="background-image: url(<?php echo sow_esc_url($img[0]) ?>)" aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>">&nbsp;</span>
+				<a href="<?php the_permalink() ?>" style="background-image: url(<?php echo sow_esc_url($img[0]) ?>)" aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>">
+					<span class="overlay"></span>
 				</a>
 			<?php else : ?>
 				<a href="<?php the_permalink() ?>" class="sow-carousel-default-thumbnail"
 				<?php echo ! empty( $default_thumbnail ) ?
-				'style="background-image: url('. sow_esc_url( $default_thumbnail ) .')"' : '' ?> aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>">&nbsp;</a>
+				'style="background-image: url('. sow_esc_url( $default_thumbnail ) .')"' : '' ?> aria-labelledby="sow-carousel-id-<?php echo the_ID(); ?>"><span class="overlay"></span></a>
 			<?php endif; ?>
 		</div>
 		<h3><a href="<?php the_permalink() ?>" id="sow-carousel-id-<?php echo the_ID(); ?>"><?php the_title() ?></a></h3>
