8ff8482e7b8b55a9c49c6075ad1c91eb815199c3
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-06-05 06:42:05 +1000

    Simple Masonry: Detect dimensions rather than using attributes
    This will avoid sizing issue with external images.

--- widgets/simple-masonry/js/simple-masonry.js
+++ widgets/simple-masonry/js/simple-masonry.js
@@ -41,7 +41,7 @@ jQuery( function ( $ ) {
 						$$.css( 'height', ( rowHeight * rowSpan ) + ( layout.gutter * ( rowSpan - 1 ) ) );
 						
 						var $img = $$.find( '> img,> a > img' );
-						var imgAR = $img.attr( 'height' ) > 0 ? $img.attr( 'width' ) / $img.attr( 'height' ) : 1;
+						var imgAR = $img.height() > 0 ? $img.width() / $img.height() : 1;
 						var itemAR = $$.height() > 0 ? $$.width() / $$.height() : 1;
 						imgAR = parseFloat( imgAR.toFixed( 3 ) );
 						itemAR = parseFloat( itemAR.toFixed( 3 ) );
@@ -51,17 +51,12 @@ jQuery( function ( $ ) {
 							$img.css( 'margin-top', '' );
 							var marginLeft = ( $img.width() - $$.width() ) * -0.5;
 							$img.css( 'margin-left', marginLeft + 'px' );
-						}
-						else {
+						} else {
 							$img.css( 'height', 'auto' );
 							$img.css( 'width', '100%' );
 							$img.css( 'margin-left', '' );
-
-							// Apply top margin if image is local
-							if ( $img.prop( 'srcset' ) && $img.prop( 'sizes' ) ) {
-								var marginTop = ( $img.height() - $$.height() ) * -0.5;
-								$img.css( 'margin-top', marginTop + 'px' );
-							}
+							var marginTop = ( $img.height() - $$.height() ) * -0.5;
+							$img.css( 'margin-top', marginTop + 'px' );
 						}
 					} );
 
