473eb28ab3e080b7e15001ffaa24b03c5fe89f46
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-08-28 22:01:30 +1000

    Post Carousel: Rename Thumbnail overlay hover color setting

--- widgets/post-carousel/post-carousel.php
+++ widgets/post-carousel/post-carousel.php
@@ -162,9 +162,9 @@ class SiteOrigin_Widget_PostCarousel_Widget extends SiteOrigin_Widget {
 				'label' => __( 'Design', 'so-widgets-bundle' ),
 				'hide' => true,
 				'fields' => array(
-					'overlay_hover_color' => array(
+					'thumbnail_overlay_hover_color' => array(
 						'type' => 'color',
-						'label' => __( 'Overlay hover color', 'so-widgets-bundle' ),
+						'label' => __( 'Thumbnail overlay hover color', 'so-widgets-bundle' ),
 						'default' => '#3279BB',
 					),
 					'navigation_color' => array(
@@ -214,7 +214,7 @@ class SiteOrigin_Widget_PostCarousel_Widget extends SiteOrigin_Widget {
 			'thumbnail_height'=> $thumb_height . 'px',
 			'thumbnail_hover_width' => $thumb_hover_width . 'px',
 			'thumbnail_hover_height'=> $thumb_hover_height . 'px',
-			'overlay_hover_color' => ! empty ( $instance['design']['overlay_hover_color'] ) ? $instance['design']['overlay_hover_color'] : '',
+			'thumbnail_overlay_hover_color' => ! empty ( $instance['design']['thumbnail_overlay_hover_color'] ) ? $instance['design']['thumbnail_overlay_hover_color'] : '',
 			'navigation_color' => ! empty ( $instance['design']['navigation_color'] ) ? $instance['design']['navigation_color'] : '',
 			'navigation_color_hover' => ! empty ( $instance['design']['navigation_color_hover'] ) ? $instance['design']['navigation_color_hover'] : '',
 			'navigation_background' => ! empty ( $instance['design']['navigation_background'] ) ? $instance['design']['navigation_background'] : '',
