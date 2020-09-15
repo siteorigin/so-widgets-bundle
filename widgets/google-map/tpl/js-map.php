0520682af4fda8e9af312d8ef4e70e390901c71b
Andrew Misplon <amisplon@gmail.com>
2020-09-15 10:50:14 +0200

    Converted spaces to tabs

--- widgets/google-map/tpl/js-map.php
+++ widgets/google-map/tpl/js-map.php
@@ -17,7 +17,7 @@
 <?php endif; ?>
 
 <div class="sow-google-map-canvas"
-     style="<?php echo ( $map_consent ) ? 'display: none;' : ''; ?>"
-     id="map-canvas-<?php echo esc_attr( $map_id ) ?>"
-     data-options="<?php echo esc_attr( json_encode( $map_data ) ) ?>"
-     data-fallback-image="<?php echo esc_attr( json_encode( $fallback_image_data ) ); ?>"></div>
+	style="<?php echo ( $map_consent ) ? 'display: none;' : ''; ?>"
+	id="map-canvas-<?php echo esc_attr( $map_id ) ?>"
+	data-options="<?php echo esc_attr( json_encode( $map_data ) ) ?>"
+	data-fallback-image="<?php echo esc_attr( json_encode( $fallback_image_data ) ); ?>"></div>
