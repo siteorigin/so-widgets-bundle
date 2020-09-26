6b78a31063f345087e821a338d13bbac76074aa2
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-09-26 10:56:36 +1000

    TinyMCE: Only Alter textarea id when field is added in repeater.

--- base/inc/fields/js/tinymce-field.js
+++ base/inc/fields/js/tinymce-field.js
@@ -123,12 +123,12 @@
 	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-tinymce', function () {
 		var $field = $( this );
 		var $parentRepeaterItem = $field.closest( '.siteorigin-widget-field-repeater-item-form' );
-		
-		// Prevent potential id overlap by appending the textarea field a random id.
-		var $textarea = $field.find( 'textarea' );
-		$textarea.attr( 'id', $textarea.attr( 'id' ) + Math.floor( Math.random() * 1000 ) );
-		
+
 		if ( $parentRepeaterItem.length > 0 ) {
+			// Prevent potential id overlap by appending the textarea field a random id.
+			var $textarea = $field.find( 'textarea' );
+			$textarea.attr( 'id', $textarea.attr( 'id' ) + Math.floor( Math.random() * 1000 ) );
+
 			if ( $parentRepeaterItem.is( ':visible' ) ) {
 				setup( $field );
 			}
