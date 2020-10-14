3cc652e63214e1a6464afe68a0aa5b82e52b7e49
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-10-15 07:04:21 +1000

    itText Compressor: Add Step Part 2
    Safari requires the step to be to the decimal point.

--- widgets/hero/hero.php
+++ widgets/hero/hero.php
@@ -241,7 +241,7 @@ class SiteOrigin_Widget_Hero_Widget extends SiteOrigin_Widget_Base_Slider {
 						'label' => __( 'FitText compressor strength', 'so-widgets-bundle' ),
 						'description' => __( 'The higher the value, the more your headings will be scaled down. Values above 1 are allowed.', 'so-widgets-bundle' ),
 						'default' => 0.85,
-						'step' => 0.1,
+						'step' => 0.01,
 						'state_handler' => array(
 							'use_fittext[show]' => array( 'show' ),
 							'use_fittext[hide]' => array( 'hide' ),
