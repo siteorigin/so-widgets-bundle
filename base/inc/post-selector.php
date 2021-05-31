7551212414194def322a8072bee140d8a6fe50a5
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-06-01 02:49:00 +1000

    Post Selector: Add option to adjust taxonomies relationship

--- base/inc/post-selector.php
+++ base/inc/post-selector.php
@@ -34,7 +34,7 @@ function siteorigin_widget_post_selector_process_query( $query, $exclude_current
 		$tax_queries = explode(',', $query['tax_query']);
 
 		$query['tax_query'] = array();
-		$query['tax_query']['relation'] = 'OR';
+		$query['tax_query']['relation'] = isset( $query['tax_query_relation'] ) ? $query['tax_query_relation'] : 'OR';
 		foreach($tax_queries as $tq) {
 			list($tax, $term) = explode(':', $tq);
 
