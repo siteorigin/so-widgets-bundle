81ef364bcba296120983481a77b2721dc6642212
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-06-03 20:09:33 +1000

    Updated label and description based on feedback

--- base/inc/fields/posts.class.php
+++ base/inc/fields/posts.class.php
@@ -54,12 +54,12 @@ class SiteOrigin_Widget_Field_Posts extends SiteOrigin_Widget_Field_Container_Ba
 
 			'tax_query_relation' => array(
 				'type' => 'radio',
-				'label' => __( 'Taxonomies Relationship', 'so-widgets-bundle' ),
+				'label' => __( 'Taxonomies relationship', 'so-widgets-bundle' ),
 				'options' => array(
 					'OR' => __( 'OR', 'so-widgets-bundle' ),
 					'AND' => __( 'AND', 'so-widgets-bundle' ),
 				),
-				'description' => __( 'The relationship between the taxonomies. AND requires posts to have all of the specified taxonomies, while OR requires posts to have at least one of the specified taxonomies.', 'so-widgets-bundle' ),
+				'description' => __( 'The relationship between taxonomies. OR requires posts to have at least one of the specified taxonomies. AND requires posts to have all of the specified taxonomies.', 'so-widgets-bundle' ),
 				'default' => 'OR',
 			),
 
