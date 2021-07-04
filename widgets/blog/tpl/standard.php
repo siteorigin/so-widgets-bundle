24d5a25f822867635720e9b0b6081ee6faeba070
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-07-05 08:03:03 +1000

    Blog: Standard Template
    Styling pending.

---
+++ widgets/blog/tpl/standard.php
@@ -0,0 +1,34 @@
+<?php if ( ! empty( $posts ) && $posts->have_posts() ) : ?>
+	<div class="sow-blog sow-blog-layout-standard sow-blog-columns-<?php echo esc_attr( $settings['column-count'] ); ?>">
+		<?php while( $posts->have_posts() ) : $posts->the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="sow-blog-header-container">
+					<?php $this->post_featured_image( $settings ); ?>
+					<header class="entry-header">
+						<?php
+						the_title(
+							'<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">',
+							'</a></h2>'
+						);
+						?>
+						<div class="entry-meta">
+							<?php $this->post_meta( $settings ); ?>
+						</div>
+					</header>
+				</div>
+
+				<div class="entry-content">
+					<?php
+						if ( $settings['content'] == 'full' ) {
+							the_content();
+						} else {
+							$this->generate_excerpt( $settings );
+						}
+					?>
+				</div>
+				<?php $this->post_footer( $settings ); ?>
+			</article>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
+<?php wp_reset_postdata(); ?>
