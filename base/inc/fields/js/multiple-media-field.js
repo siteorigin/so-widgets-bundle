8973391689ebcd47fb98ffbd7a413273f41dc41a
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-08-18 04:51:40 +1000

    Multiple Media: Repeater Support

--- base/inc/fields/js/multiple-media-field.js
+++ base/inc/fields/js/multiple-media-field.js
@@ -5,10 +5,17 @@
 	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-multiple_media', function( e ) {
 		var $field = $( this ),
 			$data = $field.find( '.siteorigin-widget-input' ),
-			selectedMedia = $data.val().split( ',' );
+			selectedMedia = $data.val().split( ',' ),
+			repeater = $data.data( 'repeater' )
 
 		if ( $field.data( 'initialized' ) ) {
 			return;
+		}
+
+		if ( repeater ) {
+			// This field is used to bulk add repeater items.
+			repeater.field = $field.siblings( '.siteorigin-widget-field-' + repeater.field ).find( ' > .siteorigin-widget-field-repeater' );
+			repeater.addBtn = repeater.field.find( '> .siteorigin-widget-field-repeater-add' );
 		}
 
 		// Handle the media uploader
@@ -40,16 +47,18 @@
 				}
 			} );
 
-			// If there's a selected image, highlight it. 
-			frame.on( 'open', function() {
-				if ( selectedMedia.length ) {
-					var selection = frame.state().get( 'selection' );
+			// If there's images selected, highlight them.
+			if ( ! repeater ) {
+				frame.on( 'open', function() {
+					if ( selectedMedia.length ) {
+						var selection = frame.state().get( 'selection' );
 
-					$.each( selectedMedia, function() {
-					    selection.add( wp.media.attachment( this ) );
-					} );
-				}
-			} );
+						$.each( selectedMedia, function() {
+						    selection.add( wp.media.attachment( this ) );
+						} );
+					}
+				} );
+			}
 
 			// Store the frame
 			$$.data( 'frame', frame );
@@ -60,35 +69,52 @@
 					attachment,
 					attachmentUrl,
 					$currentItem,
-					$thumbnail;
+					$thumbnail,
+					$inputField;
 
 				$.each( frame.state().get( 'selection' ).models, function() {
 					attachment = this.attributes;
+					if ( repeater ) {
+						// Add new item
+						repeater.addBtn.trigger( 'click' );
+						// Find image setting and set it.
+						$currentItem = repeater.field.find( '> .siteorigin-widget-field-repeater-items > .siteorigin-widget-field-repeater-item:last-of-type > .siteorigin-widget-field-repeater-item-form > .siteorigin-widget-field-' + repeater.setting );
+						$inputField = $currentItem.find( '.siteorigin-widget-input' ).not( '.media-fallback-external' );
+						$inputField.val( attachment.id );
 
-					// Don't process images that already exist.
-					if ( selectedMedia.indexOf( attachment.id.toString() ) == -1 ) {
-						$field.find( '.multiple-media-field-template .multiple-media-field-item' ).clone().appendTo( $field.find( '.multiple-media-field-items' ) );
-						$currentItem = $field.find( '.multiple-media-field-items .multiple-media-field-item' ).last();
-
-						$thumbnail = $currentItem.find( '.thumbnail' );
-						$thumbnail.attr( 'title', attachment.title );
-						$currentItem.find( '.title' ).text( attachment.title );
-
-						$currentItem.attr( 'data-id', attachment.id );
-
-						if ( typeof attachment.sizes !== 'undefined' ) {
-							if ( typeof attachment.sizes.thumbnail !== 'undefined' ) {
-								attachmentUrl = attachment.sizes.thumbnail.url;
-							} else {
-								attachmentUrl = attachment.sizes.full.url;
-							}
-						} else{
-							attachmentUrl = attachment.icon;
+						// We need to manually set the thumbnail to show as the field won't.
+						$currentItem.find( '.thumbnail' ).show();
+					} else {
+						attachmentIds.push( attachment.id );
+						// Don't process images that already exist.
+						if ( selectedMedia.indexOf( attachment.id.toString() ) == -1 ) {
+							$field.find( '.multiple-media-field-template .multiple-media-field-item' ).clone().appendTo( $field.find( '.multiple-media-field-items' ) );
+							$currentItem = $field.find( '.multiple-media-field-items .multiple-media-field-item' ).last();
+							$currentItem.attr( 'data-id', attachment.id );
+						} else {
+							return;
 						}
-						$thumbnail.attr( 'src', attachmentUrl );
 					}
 
-					attachmentIds.push( attachment.id );
+					// Display thumbnail.
+					$thumbnail = $currentItem.find( '.thumbnail' );
+					$thumbnail.attr( 'title', attachment.title );
+					$currentItem.find( '.title' ).html( attachment.title );
+					if ( typeof attachment.sizes !== 'undefined' ) {
+						if ( typeof attachment.sizes.thumbnail !== 'undefined' ) {
+							attachmentUrl = attachment.sizes.thumbnail.url;
+						} else {
+							attachmentUrl = attachment.sizes.full.url;
+						}
+					} else{
+						attachmentUrl = attachment.icon;
+					}
+					$thumbnail.attr( 'src', attachmentUrl );
+
+					if ( repeater ) {
+						// This is required to ensure state emitter titles are updated.
+						$inputField.trigger( 'change', { silent: true } );
+					}
 				} );
 
 				// Remove any no longer selected images
@@ -114,15 +140,17 @@
 			frame.open();
 		});
 
-		$( document ).on( 'click','.siteorigin-widget-field-type-multiple_media a.media-remove-button', function( e ) {
-			e.preventDefault();
-			var $currentItem = $( this ).parent();
+		if ( ! repeater ) {
+			$( document ).on( 'click','.siteorigin-widget-field-type-multiple_media a.media-remove-button', function( e ) {
+				e.preventDefault();
+				var $currentItem = $( this ).parent();
 
-			selectedMedia.splice( selectedMedia.indexOf( $currentItem.data( 'id' ) ) );
-			$data.val( selectedMedia.join( ',' ) );
+				selectedMedia.splice( selectedMedia.indexOf( $currentItem.data( 'id' ) ) );
+				$data.val( selectedMedia.join( ',' ) );
 
-			$currentItem.remove();
-		} );
+				$currentItem.remove();
+			} );
+		}
 
 		$field.data( 'initialized', true );
 	} );
