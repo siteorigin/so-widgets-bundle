03e2fdaedd4ad36cfd0432020465e26a30a7ef89
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-10-21 04:50:46 +1000

    Slider: Correct Unmute icon setting references

--- base/inc/widgets/base-slider.class.php
+++ base/inc/widgets/base-slider.class.php
@@ -35,7 +35,7 @@ abstract class SiteOrigin_Widget_Base_Slider extends SiteOrigin_Widget {
 		);
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_cycle_swipe' ) );
 
-		// Add unmute button LESS.
+		// Add Unmute icon LESS.
 		add_filter( 'siteorigin_widgets_less_variables_' . $this->id_base, array( $this, 'add_less_variables' ), 10, 3 );
 		add_filter( 'siteorigin_widgets_less_vars_' . $this->id_base, array( $this, 'add_unmute_less' ), 10, 4 );
 	}
@@ -581,7 +581,7 @@ abstract class SiteOrigin_Widget_Base_Slider extends SiteOrigin_Widget {
 	}
 
 	/**
-	 * If the unmute button is enabled, inject unmute LESS.
+	 * If the Unmute icon is enabled, inject unmute LESS.
 	 *
 	 * @param string $less The LESS content.
 	 * @param array $vars The widget LESS variables.
@@ -602,7 +602,7 @@ abstract class SiteOrigin_Widget_Base_Slider extends SiteOrigin_Widget {
 	}
 
 	/**
-	 * If the unmute button is enabled, add unmute_position LESS variable.
+	 * If the Unmute icon is enabled, add unmute_position LESS variable.
 	 *
 	 * @param array $less An array containing all LESS variables.
 	 * @param array $instance The widget instance.
