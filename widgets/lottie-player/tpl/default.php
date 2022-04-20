ecfbbb5f46a298b0dd22a188e925a7eb5497fd36
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-04-21 06:32:04 +1000

    Lottie Player: Give link a `z-index`

--- widgets/lottie-player/tpl/default.php
+++ widgets/lottie-player/tpl/default.php
@@ -10,7 +10,7 @@ if ( empty( $file ) ) {
 		<?php $bottom = ! empty( $attributes['controls'] ) ? '35px' : 0; ?>
 		<a
 			href="<?php echo sow_esc_url( $url ); ?>"
-			style="position: absolute; top: 0; right: 0; left: 0; bottom: <?php echo $bottom; ?>"
+			style="position: absolute; top: 0; right: 0; left: 0; bottom: <?php echo $bottom; ?>; z-index: 1;"
 			<?php echo $new_window ? 'target="_blank" rel="noopener noreferrer"' : ''; ?>
 		>
 			&nbsp;
