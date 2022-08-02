733e812e9e4d65de3e949767d10301c08693bdbe
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-08-02 22:59:13 +1000

    Post Carousel: Prevent Partially Visible Scrolling From Affecting Anything Carousel

--- js/carousel.js
+++ js/carousel.js
@@ -163,8 +163,8 @@ jQuery( function ( $ ) {
 						if ( $$.data( 'carousel_settings' ).loop ) {
 							$items.slick( 'slickGoTo', 0 );
 						}
-					// If the total number of slides is higher than the number of visible items, go to the last item.
-					} else if ( slidesToScroll >= numVisibleItemsFloor ) {
+					// If slidesToScroll is higher than the the number of visible items, go to the last item.
+					} else if ( $$.data( 'widget' ) == 'post' && slidesToScroll >= numVisibleItemsFloor ) {
 						// There's more slides than items, update Slick settings to allow for scrolling of partially visible items.
 						$items.slick( 'slickSetOption', 'slidesToShow', numVisibleItemsFloor );
 						$items.slick( 'slickSetOption', 'slidesToScroll', numVisibleItemsFloor );
@@ -178,7 +178,7 @@ jQuery( function ( $ ) {
 				} else if ( $( this ).hasClass( 'sow-carousel-previous' ) ) {
 					if ( $$.data( 'carousel_settings' ).loop && $items.slick( 'slickCurrentSlide' ) == 0 ) {
 						$items.slick( 'slickGoTo', lastPosition );
-					} else if ( $items.slick( 'slickCurrentSlide' ) <= slidesToScroll ) {
+					} else if ( $$.data( 'widget' ) == 'post' && $items.slick( 'slickCurrentSlide' ) <= slidesToScroll ) {
 						$items.slick( 'slickGoTo', 0 );
 					} else {
 						$items.slick( 'slickPrev' );
