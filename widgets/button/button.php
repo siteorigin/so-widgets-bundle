f87a19b8e8703342b2c50af7979d8a8b7115b890
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-10-24 07:11:34 +1300

    Button: Expand Font Size, Padding, and Rounding to Be More Customizable

--- widgets/button/button.php
+++ widgets/button/button.php
@@ -203,37 +203,34 @@ class SiteOrigin_Widget_Button_Widget extends SiteOrigin_Widget {
 					),
 
 					'font_size' => array(
-						'type' => 'select',
+						'type' => 'measurement',
 						'label' => __( 'Font Size', 'so-widgets-bundle' ),
-						'options' => array(
-							'1' => __( 'Normal', 'so-widgets-bundle' ),
-							'1.15' => __( 'Medium', 'so-widgets-bundle' ),
-							'1.3' => __( 'Large', 'so-widgets-bundle' ),
-							'1.45' => __( 'Extra large', 'so-widgets-bundle' ),
-						),
-					),
-
-					'rounding' => array(
-						'type' => 'select',
-						'label' => __( 'Rounding', 'so-widgets-bundle' ),
-						'default' => '0.25',
-						'options' => array(
-							'0' => __( 'None', 'so-widgets-bundle' ),
-							'0.25' => __( 'Slightly Rounded', 'so-widgets-bundle' ),
-							'0.5' => __( 'Very Rounded', 'so-widgets-bundle' ),
-							'1.5' => __( 'Completely Rounded', 'so-widgets-bundle' ),
-						),
+						'default' => '1em',
 					),
 
 					'padding' => array(
-						'type' => 'select',
+						'type' => 'measurement',
 						'label' => __( 'Padding', 'so-widgets-bundle' ),
-						'default' => '1',
-						'options' => array(
-							'0.5' => __( 'Low', 'so-widgets-bundle' ),
-							'1' => __( 'Medium', 'so-widgets-bundle' ),
-							'1.4' => __( 'High', 'so-widgets-bundle' ),
-							'1.8' => __( 'Very High', 'so-widgets-bundle' ),
+						'default' => '1em',
+					),
+
+					'rounding' => array(
+						'type' => 'multi-measurement',
+						'label' => __( 'Rounding', 'so-widgets-bundle' ),
+						'default' => '0.25em 0.25em 0.25em 0.25em',
+						'measurements' => array(
+							'top' => array(
+								'label' => __( 'Top', 'so-widgets-bundle' ),
+							),
+							'right' => array(
+								'label' => __( 'Right', 'so-widgets-bundle' ),
+							),
+							'bottom' => array(
+								'label' => __( 'Bottom', 'so-widgets-bundle' ),
+							),
+							'left' => array(
+								'label' => __( 'Left', 'so-widgets-bundle' ),
+							),
 						),
 					),
 
@@ -457,6 +454,19 @@ class SiteOrigin_Widget_Button_Widget extends SiteOrigin_Widget {
 			empty( $instance['design']['mobile_align'] )
 		) {
 			$instance['design']['mobile_align'] = $instance['design']['align'];
+		}
+
+		// Migrate predefined settings to more customizable settings.
+		if ( ! empty( $instance['design']['font_size'] ) && is_numeric( $instance['design']['font_size'] ) ) {
+			$instance['design']['font_size'] .= 'em';
+		}
+
+		if ( ! empty( $instance['design']['padding'] ) && is_numeric( $instance['design']['padding'] ) ) {
+			$instance['design']['padding'] .= 'em';
+		}
+
+		if ( ! empty( $instance['design']['rounding'] ) && is_numeric( $instance['design']['rounding'] ) ) {
+			$instance['design']['rounding'] = $instance['design']['rounding'] . 'em ' . $instance['design']['rounding'] . 'em ' . $instance['design']['rounding'] . 'em ' . $instance['design']['rounding'] . 'em';
 		}
 
 		return $instance;
