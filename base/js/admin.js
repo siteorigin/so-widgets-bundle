bd3623ff65c0e9716de086f48ae742b2e0b715b2
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-12-21 16:12:43 +1300

    Widgets Validation Dev Hook: Only Pass `id-base` if exists

--- base/js/admin.js
+++ base/js/admin.js
@@ -1565,7 +1565,7 @@ var sowbForms = window.sowbForms || {};
 				valid,
 				form,
 				// Widget ID.
-				form.find( '.siteorigin-widget-form' ).data( 'id-base' )
+				typeof jQuery( '.widget-content' ).data( 'test' ) !== 'undefined' ? form.find( '.siteorigin-widget-form' ).data( 'id-base' ) : ''
 			]
 		);
 
