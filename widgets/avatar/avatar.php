291a53cbdbcb15b74a4015b171ef47f264cbcc50
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-01-31 03:15:44 +1300

    Add Avatar Widget

---
+++ widgets/avatar/avatar.php
@@ -0,0 +1,201 @@
+<?php
+/*
+Widget Name: Avatar
+Description: Placeholder.
+Author: SiteOrigin
+Author URI: https://siteorigin.com
+Documentation: https://siteorigin.com/widgets-bundle/avatar-widget/
+*/
+
+class SiteOrigin_Widget_Avatar_Widget extends SiteOrigin_Widget {
+public function __construct() {
+		parent::__construct(
+			'sow-avatar',
+			__( 'SiteOrigin Avatar', 'so-widgets-bundle' ),
+			array(
+				'description' => __( 'Placeholder', 'so-widgets-bundle' ),
+				'help' => 'https://siteorigin.com/widgets-bundle/avatar-widget/',
+			),
+			array(),
+			false,
+			plugin_dir_path( __FILE__ )
+		);
+	}
+
+	public function get_widget_form() {
+		return array(
+			'avatar' => array(
+				'type' => 'checkbox',
+				'default' => true,
+				'label' => __( 'Show Author Avatar', 'so-widgets-bundle' ),
+			),
+			'link_avatar' => array(
+				'type' => 'checkbox',
+				'default' => true,
+				'label' => __( 'Link Author Avatar', 'so-widgets-bundle' ),
+			),
+			'link_name' => array(
+				'type' => 'checkbox',
+				'default' => true,
+				'label' => __( 'Link Author Name', 'so-widgets-bundle' ),
+			),
+			'link_all_posts' => array(
+				'type' => 'checkbox',
+				'default' => true,
+				'label' => __( 'Add All Posts by Author Link', 'so-widgets-bundle' ),
+			),
+			'author_bio' => array(
+				'type' => 'checkbox',
+				'default' => true,
+				'label' => __( 'Show Author Bio', 'so-widgets-bundle' ),
+			),
+			'design' => array(
+				'type' => 'section',
+				'label' => __( 'Design', 'so-widgets-bundle' ),
+				'hide' => true,
+				'fields' => array(
+					'container' => array(
+						'type' => 'section',
+						'label' => __( 'Container', 'so-widgets-bundle' ),
+						'hide' => true,
+						'fields' => array(
+							'background_color' => array(
+								'type' => 'color',
+								'label' => __( 'Background', 'so-widgets-bundle' ),
+								'default' => '#000',
+							),
+							'background_opacity' => array(
+								'type' => 'slider',
+								'label' => __( 'Background Opacity', 'so-widgets-bundle' ),
+								'min' => 0,
+								'max' => 1,
+								'step' => 0.01,
+								'default' => 0.05,
+							),
+						),
+					),
+					'name' => array(
+						'type' => 'section',
+						'label' => __( 'Author Name', 'so-widgets-bundle' ),
+						'hide' => true,
+						'fields' => array(
+							'font' => array(
+								'type' => 'font',
+								'label' => __( 'Font', 'so-widgets-bundle' ),
+							),
+							'font_size' => array(
+								'type' => 'measurement',
+								'label' => __( 'Font Size', 'so-widgets-bundle' ),
+								'default' => '18px',
+							),
+							'color' => array(
+								'type' => 'color',
+								'label' => __( 'Color', 'so-widgets-bundle' ),
+							),
+							'color_hover' => array(
+								'type' => 'color',
+								'label' => __( 'Hover Color', 'so-widgets-bundle' ),
+							),
+						),
+					),
+					'bio' => array(
+						'type' => 'section',
+						'label' => __( 'Author Bio', 'so-widgets-bundle' ),
+						'hide' => true,
+						'fields' => array(
+							'font' => array(
+								'type' => 'font',
+								'label' => __( 'Font', 'so-widgets-bundle' ),
+							),
+							'font_size' => array(
+								'type' => 'measurement',
+								'label' => __( 'Font Size', 'so-widgets-bundle' ),
+							),
+							'color' => array(
+								'type' => 'color',
+								'label' => __( 'Color', 'so-widgets-bundle' ),
+							),
+							'link' => array(
+								'type' => 'color',
+								'label' => __( 'Link Color', 'so-widgets-bundle' ),
+							),
+							'link_hover' => array(
+								'type' => 'color',
+								'label' => __( 'Link Hover Color', 'so-widgets-bundle' ),
+							),
+						),
+					),
+				),
+			),
+		);
+	}
+
+	public function get_template_variables( $instance, $args ) {
+		return array(
+			'show_avatar' => ! empty( $instance['avatar'] ),
+			'link_avatar' => ! empty( $instance['link_avatar'] ),
+			'link_name' => ! empty( $instance['link_name'] ),
+			'author_bio' => ! empty( $instance['author_bio'] ),
+			'link_all_posts' => ! empty( $instance['link_all_posts'] ),
+		);
+	}
+
+	public function get_less_variables( $instance ) {
+		if ( empty( $instance ) ) {
+			return array();
+		}
+
+		$less_vars = array();
+
+		$color = ! empty( $instance['design']['container']['background_color'] ) ? $instance['design']['container']['background_color'] : '#000';
+		$rgb = ltrim( $color, '#' );
+		$rgb = array_map( 'hexdec', str_split( $rgb, strlen( $rgb ) == 6 ? 2 : 1 ) );
+		$opacity = ! empty( $instance['design']['container']['background_opacity'] ) ? $instance['design']['container']['background_opacity'] : 0.05;
+		$less_vars['container_background'] = "rgba( $rgb[0], $rgb[1], $rgb[2], $opacity )";
+
+		if ( ! empty( $instance['design']['name']['font'] ) ) {
+			$font = siteorigin_widget_get_font( $instance['design']['name']['font'] );
+			$less_vars['name_font'] = $font['family'];
+
+			if ( ! empty( $font['weight'] ) ) {
+				$less_vars['name_font_style'] = $font['style'];
+				$less_vars['name_font_weight'] = $font['weight_raw'];
+			}
+		}
+		$less_vars['name_font_size'] = ! empty( $instance['design']['name']['font_size'] ) ? $instance['design']['name']['font_size'] : '';
+		$less_vars['name_color'] = ! empty( $instance['design']['name']['color'] ) ? $instance['design']['name']['color'] : '';
+		$less_vars['name_color_hover'] = ! empty( $instance['design']['name']['color_hover'] ) ? $instance['design']['name']['color_hover'] : '';
+
+		if ( ! empty( $instance['design']['bio']['font'] ) ) {
+			$font = siteorigin_widget_get_font( $instance['design']['bio']['font'] );
+			$less_vars['bio_font'] = $font['family'];
+
+			if ( ! empty( $font['weight'] ) ) {
+				$less_vars['bio_font_style'] = $font['style'];
+				$less_vars['bio_font_weight'] = $font['weight_raw'];
+			}
+		}
+		$less_vars['bio_font_size'] = ! empty( $instance['design']['bio']['font_size'] ) ? $instance['design']['bio']['font_size'] : '';
+		$less_vars['bio_color'] = ! empty( $instance['design']['bio']['color'] ) ? $instance['design']['bio']['color'] : '';
+		$less_vars['bio_link'] = ! empty( $instance['design']['bio']['link'] ) ? $instance['design']['bio']['link'] : '';
+		$less_vars['bio_link_hover'] = ! empty( $instance['design']['bio']['link_hover'] ) ? $instance['design']['bio']['link_hover'] : '';
+
+		return $less_vars;
+	}
+
+	public function get_form_teaser() {
+		if ( class_exists( 'SiteOrigin_Premium' ) ) {
+			return false;
+		}
+
+		return array(
+			sprintf(
+				__( 'Placeholder teaser for %sSiteOrigin Premium%s', 'so-widgets-bundle' ),
+				'<a href="https://siteorigin.com/downloads/premium/?featured_addon=plugin/addon-link" target="_blank" rel="noopener noreferrer">',
+				'</a>'
+			),
+		);
+	}
+}
+
+siteorigin_widget_register( 'sow-avatar', __FILE__, 'SiteOrigin_Widget_Avatar_Widget' );
---
+++ widgets/avatar/tpl/default.php
@@ -0,0 +1,47 @@
+<div class="sow-author-box">
+	<?php if ( $show_avatar ) { ?>
+		<div class="sow-author-avatar">
+			<?php if ( $link_avatar ) { ?>
+				<a href="<?php echo esc_urL( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+			<?php
+			}
+
+			echo get_avatar(
+				get_the_author_meta( 'ID' ),
+				apply_filters( 'siteorigin_widgets_avatar_size', 100 )
+			);
+
+		if ( $link_avatar ) {
+			?>
+				</a>
+			<?php } ?>
+		</div>
+	<?php } ?>
+	<div class="sow-author-description">
+		<h4 class="sow-author-title">
+			<small class="sow-author-info">
+				<?php if ( $link_name ) { ?>
+					<a href="<?php echo esc_urL( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+				<?php
+				}
+				echo get_the_author();
+
+				if ( $link_name ) {
+					?>
+					</a>
+				<?php } ?>
+
+				<?php if ( $link_all_posts ) { ?>
+					<a href="<?php echo esc_urL( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+						<?php echo __( sprintf( 'View posts by %s', get_the_author() ), 'so-widgets-bundle' ); ?> 
+					</a>
+				<?php } ?>
+			</small>
+		</h4>
+		<?php if ( $author_bio ) { ?>
+			<div class="sow-author-bio">
+				<?php echo wp_kses_post( get_the_author_meta( 'description' ), null ); ?>
+			</div>
+		<?php } ?>
+	</div>
+</div>
