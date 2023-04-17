8c1c1e18172cef0c32065662290e69b520e72231
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-04-18 06:07:09 +1200

    Author Box: Add Template Hooks

--- widgets/author-box/tpl/default.php
+++ widgets/author-box/tpl/default.php
@@ -1,6 +1,8 @@
 <div class="sow-author-box">
+	<?php do_action( 'siteorigin_widgets_author_box_before', $instance ); ?>
 	<?php if ( $show_avatar ) { ?>
 		<div class="sow-author-box-avatar">
+			<?php do_action( 'siteorigin_widgets_author_box_avatar_above', $instance ); ?>
 			<?php if ( $link_avatar ) { ?>
 				<a href="<?php echo esc_urL( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
 			<?php
@@ -15,9 +17,12 @@
 			?>
 				</a>
 			<?php } ?>
+			<?php do_action( 'siteorigin_widgets_author_box_avatar_below', $instance ); ?>
 		</div>
 	<?php } ?>
 	<div class="sow-author-box-description">
+		<?php do_action( 'siteorigin_widgets_author_box_description_above', $instance ); ?>
+
 		<h4 class="sow-author-box-title">
 			<small class="sow-author-box-info">
 				<?php if ( $link_name ) { ?>
@@ -37,11 +42,19 @@
 					</a>
 				<?php } ?>
 			</small>
+
+			<?php do_action( 'siteorigin_widgets_author_box_description_inline', $instance ); ?>
 		</h4>
+
 		<?php if ( $author_bio ) { ?>
 			<div class="sow-author-box-bio">
+		<?php do_action( 'siteorigin_widgets_author_box_description_bio_before', $instance ); ?>
 				<?php echo wp_kses_post( get_the_author_meta( 'description' ), null ); ?>
 			</div>
+			<?php do_action( 'siteorigin_widgets_author_box_description_bio_after', $instance ); ?>
 		<?php } ?>
+
+		<?php do_action( 'siteorigin_widgets_author_box_description_below', $instance ); ?>
 	</div>
+	<?php do_action( 'siteorigin_widgets_author_box_after', $instance ); ?>
 </div>
