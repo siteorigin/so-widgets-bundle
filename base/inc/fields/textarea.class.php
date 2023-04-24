2d2d208c2aaa228371ed43df616600f468e45114
Joe <46725266+pobrsan@users.noreply.github.com>
2023-04-24 10:34:33 -0400

    Update textarea.class.php to dedupe editor
    Looks like an old version of the textarea layout was left in the class and causing a duplicate textarea in the WordPress editor when textarea was in a widget.

--- base/inc/fields/textarea.class.php
+++ base/inc/fields/textarea.class.php
@@ -14,19 +14,6 @@ class SiteOrigin_Widget_Field_Textarea extends SiteOrigin_Widget_Field_Text_Inpu
 	protected function render_field( $value, $instance ) {
 		?>
 		<textarea
-		type="text"
-		name="<?php echo esc_attr( $this->element_name ) ?>"
-		id="<?php echo esc_attr( $this->element_id ) ?>"
-		<?php
-		$this->render_CSS_classes( $this->get_input_classes() );
-
-			if ( ! empty( $this->placeholder ) ) {
-				echo 'placeholder="' . esc_attr( $this->placeholder ) . '"';
-		}
-			if ( ! empty( $this->readonly ) ) {
-				echo 'readonly';
-			} ?>><?php echo esc_textarea( $value ); ?></textarea>
-		<textarea
 			type="text"
 			name="<?php echo esc_attr( $this->element_name ); ?>"
 			id="<?php echo esc_attr( $this->element_id ); ?>"
