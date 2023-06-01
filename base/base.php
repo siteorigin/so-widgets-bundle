fd74fd8d3a4824a4e698420584fbe3c24440fba0
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-06-02 02:27:14 +1200

    Image Widget: Add Image Shape Support

--- base/base.php
+++ base/base.php
@@ -13,6 +13,7 @@ include plugin_dir_path( __FILE__ ) . 'inc/actions.php';
 include plugin_dir_path( __FILE__ ) . 'inc/shortcode.php';
 include plugin_dir_path( __FILE__ ) . 'inc/video.php';
 include plugin_dir_path( __FILE__ ) . 'inc/routes/sowb-rest-routes.php';
+include plugin_dir_path( __FILE__ ) . 'inc/shapes/shapes.php';
 
 function siteorigin_widget_add_inline_css( $css ) {
 	global $siteorigin_widgets_inline_styles;
---
+++ base/inc/shapes/shapes.php
@@ -0,0 +1,42 @@
+<?php
+
+function siteorigin_widgets_image_shapes( $shape = false) {
+	$shapes = apply_filters(
+		'siteorigin_widgets_image_shapes',
+		array(
+			'circle' => __( 'Circle', 'so-widgets-bundle'  ),
+			'oval' => __( 'Oval', 'so-widgets-bundle'  ),
+			'triangle' => __( 'Triangle', 'so-widgets-bundle'  ),
+			'square' => __( 'Square', 'so-widgets-bundle'  ),
+			'diamond' => __( 'Diamond', 'so-widgets-bundle'  ),
+			'rhombus' => __( 'Rhombus', 'so-widgets-bundle'  ),
+			'parallelogram' => __( 'Parallelogram', 'so-widgets-bundle'  ),
+			'pentagon' => __( 'Pentagon', 'so-widgets-bundle'  ),
+			'hexagon' => __( 'Hexagon', 'so-widgets-bundle'  ),
+		)
+	);
+
+	if ( ! $shape ) {
+		return $shapes;
+	} elseif ( isset( $shapes[ $shape ] ) ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+add_filter( 'siteorigin_widgets_icon_families', 'siteorigin_widgets_icon_families_filter' );
+
+
+function siteorigin_widgets_image_shape( $shape ) {
+	$shapes = siteorigin_widgets_image_shapes( $shape );
+	if ( siteorigin_widgets_image_shapes( $shape ) ) {
+		$file = wp_normalize_path( apply_filters( 'siteorigin_widgets_image_shape_file_path', plugin_dir_path( __FILE__ ) . '/images/' . $shape . '.png' ) ); 
+		if ( file_exists( $file ) ) {
+			return wp_normalize_path( apply_filters( 'siteorigin_widgets_image_shape_file_url', plugin_dir_url( __FILE__ ) . '/images/' . $shape . '.png' ) );
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
