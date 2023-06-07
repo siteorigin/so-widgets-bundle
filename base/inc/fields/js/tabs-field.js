cea60def10df788e8c01077c13393eda35c15df9
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-06-08 01:09:00 +1200

    Tabs Form Field: If Only One Tab, Show Standard Section

--- base/inc/fields/js/tabs-field.js
+++ base/inc/fields/js/tabs-field.js
@@ -9,16 +9,24 @@
 			return;
 		}
 
-		$field.find( '.siteorigin-widget-tabs > li' ).on( 'click', function( e ) {
-			var $$ = $( this );
-			e.preventDefault();
-			if ( ! $$.hasClass( 'sow-active-tab' ) ) {
-				$( '.sow-active-tab' ).removeClass( 'sow-active-tab' );
-				$( '.siteorigin-widget-field-' + $$.data( 'id' ) ).find( '> .siteorigin-widget-section' ).addClass( 'sow-active-tab' );
-				$$.addClass( 'sow-active-tab' );
-			}
-		} );
-		$field.find( '.siteorigin-widget-tabs > li:first-of-type' ).trigger( 'click' );
+		$items = $field.find( '.siteorigin-widget-tabs > li' );
+		if ( $items.length == 1 ) {
+			// There's only one tab. Show the linked section as a standard tab.
+			$( '.siteorigin-widget-field-' + $items.data( 'id' ) ).find( '> .siteorigin-widget-field-label ' ).removeClass( 'siteorigin-widget-section-tab' );
+		} else {
+			$items.on( 'click', function( e ) {
+				var $$ = $( this );
+				e.preventDefault();
+				if ( ! $$.hasClass( 'sow-active-tab' ) ) {
+					$( '.sow-active-tab' ).removeClass( 'sow-active-tab' );
+					$( '.siteorigin-widget-field-' + $$.data( 'id' ) ).find( '> .siteorigin-widget-section' ).addClass( 'sow-active-tab' );
+					$$.addClass( 'sow-active-tab' );
+				}
+			} );
+			$items.first().trigger( 'click' );
+
+		}
+
 
 		$field.data( 'initialized', true );
 	} );
