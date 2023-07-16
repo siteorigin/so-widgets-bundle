3c634cdda7312bf83b6a6662930c1b9f46557834
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-07-16 23:59:24 +1200

    Image: Prevent Image Shapes Being Enabled When They Shouldn't Be

--- widgets/image/image.php
+++ widgets/image/image.php
@@ -370,7 +370,12 @@ class SiteOrigin_Widget_Image_Widget extends SiteOrigin_Widget {
 			'responsive_breakpoint' => $this->get_global_settings( 'responsive_breakpoint' ),
 		);
 
-		if ( ! empty( $instance['image_shape'] ) &&  SiteOrigin_Widget_Image_Shapes::single()->is_valid_shape( $instance['image_shape']['shape'] ) ) {
+		if (
+			! empty( $instance['image_shape'] ) &&
+			! empty( $instance['image_shape']['enable'] ) &&
+			SiteOrigin_Widget_Image_Shapes::single()->is_valid_shape( $instance['image_shape']['shape'] )
+		) {
+
 			$less_variables['image_shape_size'] = ! empty( $instance['image_shape']['size'] ) ? $instance['image_shape']['size'] : 'contain';
 			$less_variables['image_shape_repeat'] = ! empty( $instance['image_shape']['repeat'] ) ? $instance['image_shape']['repeat'] : 'no-repeat';
 			$less_variables['image_shape_position'] = ! empty( $instance['image_shape']['alignment'] ) ? $instance['image_shape']['alignment'] : 'center';
