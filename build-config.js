aaa5ffff624b55347f7f65b0de3bf1fbb1f5837c
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-08-13 02:57:26 +1200

    Installer: Exclude Standalone Updater

--- build-config.js
+++ build-config.js
@@ -41,19 +41,20 @@ module.exports = {
     },
     copy: {
         src: [
-            '**/!(*.js|*.less|*.css)',          // Everything except .js, .css and .less files
-            'icons/**/*css',                    // Copy CSS for icon packs.
-            'js/lib/**/*css',                   // Copy CSS for JS libs.
-            'base/less/*.less',                 // LESS libraries used in runtime styles
+            '**/!(*.js|*.less|*.css)',                             // Everything except .js, .css and .less files
+            'icons/**/*css',                                      // Copy CSS for icon packs.
+            'js/lib/**/*css',                                     // Copy CSS for JS libs.
+            'base/less/*.less',                                   // LESS libraries used in runtime styles
             'base/inc/widgets/less/*.less',
-            'base/inc/installer/css/*css',      // Include Installer CSS.
-            'widgets/**/styles/*.less',         // All the widgets' runtime .less files
-            '!{build,build/**}',                // Ignore build/ and contents
-            '!{tmp,tmp/**}',                    // Ignore tmp/ and contents
-            '!so-widgets-bundle.php',           // Not the base plugin file. It is copied by the 'version' task.
-            '!readme.txt',                      // Not the readme.txt file. It is copied by the 'version' task.
-            '!readme.md',                       // Ignore the readme.md file. It is for the github repo.
-            '!.editorconfig',                   // Ignore .editorconfig file. Only for development.
+            'base/inc/installer/css/*css',                        // Include Installer CSS.
+            'widgets/**/styles/*.less',                           // All the widgets' runtime .less files
+            '!{build,build/**}',                                  // Ignore build/ and contents
+            '!{tmp,tmp/**}',                                      // Ignore tmp/ and contents
+            '!so-widgets-bundle.php',                             // Not the base plugin file. It is copied by the 'version' task.
+            '!readme.txt',                                        // Not the readme.txt file. It is copied by the 'version' task.
+            '!readme.md',                                         // Ignore the readme.md file. It is for the github repo.
+            '!.editorconfig',                                     // Ignore .editorconfig file. Only for development.
+            '!base/inc/installer/inc/github-plugin-updater.php',  // Exclude Installer's  Updater.
         ]
     },
     i18n: {
