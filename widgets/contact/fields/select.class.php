6b4af402e9ac0295071eef7b14841e33771cb26d
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-09-05 01:33:54 +1200

    Contact Select Multiple: Remove Placeholder to Prevent Empty Space

--- widgets/contact/fields/select.class.php
+++ widgets/contact/fields/select.class.php
@@ -14,14 +14,18 @@ class SiteOrigin_Widget_ContactForm_Field_Select extends SiteOrigin_Widget_Conta
 			<?php echo ! empty( $options['field']['multiple_select'] ) ? 'multiple' : ''; ?>
 		>
 			<?php
-			if ( $options['show_placeholder'] ) {
+			if ( $options['show_placeholder'] && empty( $options['field']['multiple_select'] ) ) {
 				?>
 				<option selected disabled><?php esc_html_e( $options['field']['label'] ); ?></option>
 				<?php
 			}
 
 			if ( ! empty( $options['field']['options'] ) ) {
-				if ( ! $options['show_placeholder'] && $options['field']['required']['required'] ) {
+				if (
+					! $options['show_placeholder'] &&
+					$options['field']['required']['required'] &&
+					empty( $options['field']['multiple_select'] )
+				) {
 					?>
 					<option selected <?php if ( ! $options['field']['required']['required'] ) {
 						echo 'disabled';
