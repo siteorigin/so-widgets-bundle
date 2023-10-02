60900428a1f817169a358ef889a4c33b1c4bd970
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-10-03 00:44:08 +1300

    Revert "Features: Use `gap` For Spacing Between Items"
    This reverts commit cf67a7ddd8b1d900fa22267130af562bfeaceaaa.

--- widgets/features/tpl/default.php
+++ widgets/features/tpl/default.php
@@ -4,8 +4,6 @@ $per_row = ! empty( $instance['per_row'] ) ? $instance['per_row'] : 3;
 if ( ! empty( $instance['features'] ) ) {
 	$last_row = floor( ( count( $instance['features'] ) - 1 ) / $per_row );
 }
-$feature_gap = empty( $instance['feature_space'] ) ? '25' : $instance['feature_space'];
-$feature_width = 'calc(' . round( 100 / $per_row, 3 ) . '% - ' . $feature_gap . ')';
 ?>
 
 <div class="sow-features-list <?php if ( $instance['responsive'] ) {
@@ -26,7 +24,7 @@ $feature_width = 'calc(' . round( 100 / $per_row, 3 ) . '% - ' . $feature_gap . 
 			?>
 			<div
 				class="sow-features-feature sow-icon-container-position-<?php echo esc_attr( $feature['container_position'] ); ?> <?php if ( floor( $i / $per_row ) == $last_row ) echo 'sow-features-feature-last-row'; ?>"
-				style="display: flex; flex-direction: <?php echo $this->get_feature_flex_direction( $feature['container_position'], ! empty( $instance['more_text_bottom_align'] ) ); ?>; width: <?php echo $feature_width; ?>;"
+				style="display: flex; flex-direction: <?php echo $this->get_feature_flex_direction( $feature['container_position'], ! empty( $instance['more_text_bottom_align'] ) ); ?>; width: <?php echo round( 100 / $per_row, 3 ); ?>%;"
 			>
 			<?php if ( $right_left_read_more ) { ?>
 				<div class="sow-features-feature-right-left-container" style="display: flex; flex-direction: inherit;">
