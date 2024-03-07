94ef583b1831f0a835f0c51634c422ab57c76298
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2024-03-08 05:04:39 +1300

    Recent Posts: Spacing & Escaping Translation

--- widgets/author-box/tpl/default.php
+++ widgets/author-box/tpl/default.php
@@ -52,16 +52,14 @@
 		<div class="sow-author-box-info">
 			<?php if ( $link_all_posts ) { ?>
 				<a href="<?php echo esc_urL( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" class="sow-author-box-author-all">
-					<?php echo __( sprintf( 'View posts by %s', get_the_author() ), 'so-widgets-bundle' ); ?> 
+					<?php esc_html_e( sprintf( 'View posts by %s', get_the_author() ), 'so-widgets-bundle' ); ?> 
 				</a>
 			<?php } ?>
 		</div>
 
-
-
 		<?php if ( $author_bio ) { ?>
 			<div class="sow-author-box-bio">
-		<?php do_action( 'siteorigin_widgets_author_box_description_bio_before', $instance ); ?>
+				<?php do_action( 'siteorigin_widgets_author_box_description_bio_before', $instance ); ?>
 				<?php echo wp_kses_post( get_the_author_meta( 'description' ), null ); ?>
 			</div>
 			<?php do_action( 'siteorigin_widgets_author_box_description_bio_after', $instance ); ?>
