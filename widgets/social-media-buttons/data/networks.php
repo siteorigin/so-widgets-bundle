d8624a263ccb6ff283faf9e34dfe5066361bf1a3
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2024-05-08 00:34:57 +1200

    Social Buttons: Change Instagram Background Color

--- widgets/social-media-buttons/data/networks.php
+++ widgets/social-media-buttons/data/networks.php
@@ -71,7 +71,7 @@ return array(
 		'label' => __( 'Instagram', 'so-widgets-bundle' ),
 		'base_url' => 'https://instagram.com/',
 		'icon_color' => '#ffffff',
-		'button_color' => '#3d739c',
+		'button_color' => '#405DE6',
 	),
 	'vk' => array(
 		'label' => __( 'VK', 'so-widgets-bundle' ),
