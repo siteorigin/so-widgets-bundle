aa6c83fb1939d5e03671d097f0a19ac564afcb54
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2024-05-13 03:12:10 +1200

    Recent Posts: Further Setting ids Adjustments to Avoid Conflict

--- widgets/recent-posts/tpl/default.php
+++ widgets/recent-posts/tpl/default.php
@@ -16,14 +16,14 @@ if ( $query->have_posts() ) {
 			<li class="sow-recent-posts-item">
 				<div class="sow-recent-posts-item-inner">
 					<?php do_action( 'siteorigin_widgets_recent_posts_item_start', $instance ); ?>
-					<?php SiteOrigin_Widget_Recent_Posts_Widget::featured_image( $settings ); ?>
+					<?php SiteOrigin_Widget_Recent_Posts_Widget::featured_image( $recent_settings ); ?>
 
 					<div class="sow-recent-posts-item-hgroup">
 						<?php
-						SiteOrigin_Widget_Recent_Posts_Widget::post_title( $settings );
-						SiteOrigin_Widget_Recent_Posts_Widget::post_date( $settings );
-						SiteOrigin_Widget_Recent_Posts_Widget::content( $settings );
-						SiteOrigin_Widget_Recent_Posts_Widget::read_more( $settings );
+						SiteOrigin_Widget_Recent_Posts_Widget::post_title( $recent_settings );
+						SiteOrigin_Widget_Recent_Posts_Widget::post_date( $recent_settings );
+						SiteOrigin_Widget_Recent_Posts_Widget::content( $recent_settings );
+						SiteOrigin_Widget_Recent_Posts_Widget::read_more( $recent_settings );
 						?>
 					</div>
 					<?php do_action( 'siteorigin_widgets_recent_posts_item_end', $instance ); ?>
