8d77724aca6f6f8d0f0a2fc242b9af10bc21640e
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-05-15 00:16:32 +1200

    Set up fields within the Toggle Field.

--- base/inc/fields/js/toggle-field.js
+++ base/inc/fields/js/toggle-field.js
@@ -11,12 +11,20 @@
 		const $label = $field.find( '.sowb-toggle-switch' );
 		const $container = $field.find( '.siteorigin-widget-toggle' );
 
+		setTimeout( () => {
+			// If the Toggle state is enabled, set up the fields within it.
+			if ( $state.is( ':checked' ) ) {
+				$container.sowSetupForm();
+			}
+		}, 0 );
+
 		$state.on( 'change', () => {
 			const status = $state.is( ':checked' );
 
 			if ( status ) {
 				$container.slideDown( 200 );
 				$label.addClass( 'sowb-toggled-on' ).removeClass( 'sowb-toggled-off' );
+				$container.sowSetupForm();
 			} else {
 				$container.slideUp( 200 );
 				$label.addClass( 'sowb-toggled-off' ).removeClass( 'sowb-toggled-on' );
