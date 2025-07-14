c930240cc1c353673415b5cc54cef7728089fb3a
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-07-15 02:00:27 +1200

    Site Editor: Add Image Radio Form Field Support

--- base/inc/fields/js/image-radio-field.js
+++ base/inc/fields/js/image-radio-field.js
@@ -2,17 +2,24 @@
 
 ( function( $ ) {
 
-	$( document ).on( 'sowsetupform', function( e ) {
-		var $form = $( e.target );
-
-		$form.find( '.siteorigin-widget-field-type-image-radio' ).each( function() {
-			var $$ = $( this );
+	const setupRadioField = function( e ) {
+		const $$ = $( this );
+		$$.find( 'input[type="radio"]:checked' ).parent().addClass( 'so-selected' );
+		$$.find( 'input[type="radio"]' ).on( 'change', function() {
+			$$.find( 'input[type="radio"]' ).parent().removeClass( 'so-selected' );
 			$$.find( 'input[type="radio"]:checked' ).parent().addClass( 'so-selected' );
-			$$.find( 'input[type="radio"]' ).on( 'change', function() {
-				$$.find( 'input[type="radio"]' ).parent().removeClass( 'so-selected' );
-				$$.find( 'input[type="radio"]:checked' ).parent().addClass( 'so-selected' );
-			} );
 		} );
+	};
+
+	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-image-radio', setupRadioField );
+
+	// Add support for the Site Editor.
+	window.addEventListener( 'message', function( e ) {
+		if ( e.data && e.data.action === 'sowbBlockFormInit' ) {
+			$( '.siteorigin-widget-field-type-image-radio' ).each( function() {
+				setupRadioField.call( this );
+			} );
+		}
 	} );
 
 }( jQuery ) );
