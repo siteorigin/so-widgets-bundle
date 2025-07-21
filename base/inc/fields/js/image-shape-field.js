8d0611e73bc4e5361881aa252df713393f206e9d
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-07-22 00:08:09 +1200

    Site Editor: Add Image Shape Form Field Support

--- base/inc/fields/js/image-shape-field.js
+++ base/inc/fields/js/image-shape-field.js
@@ -1,19 +1,18 @@
 /* global jQuery, soWidgets */
 
 ( function( $ ) {
-
-	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-image_shape', function( e ) {
+	const setupImageShapeField = function( e ) {
 		if ( $( this ).data( 'initialized' ) ) {
 			return;
 		}
 
-		var $field = $( this ),
+		const $field = $( this ),
 			$search = $field.find( '.siteorigin-widget-shape-search' ),
 			$shapes = $field.find( '.siteorigin-widget-shapes' ),
 			$data = $field.find( '.siteorigin-widget-input' ),
 			$current = $field.find( '.siteorigin-widget-shape-current' );
 
-		var loadActiveShape = function( $shape ) {
+		const loadActiveShape = function( $shape ) {
 			$shape.addClass( 'selected' );
 			$current.find( '.siteorigin-widget-shape' ).html( $shape.find( '.siteorigin-widget-shape-image' ).clone() );
 			$current.find( '.siteorigin-widget-shape-image' ).data( 'shape', $shape.data( 'shape' ) )
@@ -29,7 +28,7 @@
 
 		// Handle shape selection.
 		$shapes.find( '.siteorigin-widget-shape' ).on( 'click', function() {
-			var $$ = $( this );
+			const $$ = $( this );
 			$field.find( '.siteorigin-widget-shape' ).removeClass( 'selected' );
 			loadActiveShape( $$ );
 			$data.val( $$.data( 'shape' ) );
@@ -38,12 +37,12 @@
 
 		// Handle searching for shapes.
 		$search.on( 'keyup change', function() {
-			var searchVal = $search.val().toLowerCase();
+			const searchVal = $search.val().toLowerCase();
 			if ( searchVal === '' ) {
 				$shapes.find( '.siteorigin-widget-shape' ).show();
 			} else {
 				$shapes.find( '.siteorigin-widget-shape' ).each( function() {
-					var $$ = $( this );
+					const $$ = $( this );
 					if ( $$.data( 'shape' ).indexOf( searchVal ) === -1 ) {
 						$$.hide();
 					} else {
@@ -56,6 +55,17 @@
 		loadActiveShape( $shapes.find( '.siteorigin-widget-shape[data-shape="' + ( $data.val() !== '' ? $data.val() : 'circle' ) + '"]' ) );
 
 		$field.data( 'initialized', true );
+	};
+
+	$( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-image_shape', setupImageShapeField );
+
+	// Add support for the Site Editor.
+	window.addEventListener( 'message', function( e ) {
+		if ( e.data && e.data.action === 'sowbBlockFormInit' ) {
+			$( '.siteorigin-widget-field-type-image_shape' ).each( function() {
+				setupImageShapeField.call( this );
+			} );
+		}
 	} );
 
 } )( jQuery );
