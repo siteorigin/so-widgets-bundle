e529553af5cfaae36152c9fb321853ef7fd40d52
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-08-22 04:01:54 +1200

    Add Package.json
    Uses https://github.com/siteorigin/siteorigin-tests-common

--- build-config.js
+++ build-config.js
@@ -50,12 +50,14 @@ module.exports = {
             'base/inc/installer/css/*css',                        // Include Installer CSS.
             'widgets/**/styles/*.less',                           // All the widgets' runtime .less files
             '!{build,build/**}',                                  // Ignore build/ and contents
+			'!{node_modules,node_modules/**}',                    // Ignore node_modules/ and contents.
             '!{tmp,tmp/**}',                                      // Ignore tmp/ and contents
             '!so-widgets-bundle.php',                             // Not the base plugin file. It is copied by the 'version' task.
             '!readme.txt',                                        // Not the readme.txt file. It is copied by the 'version' task.
             '!readme.md',                                         // Ignore the readme.md file. It is for the github repo.
             '!.editorconfig',                                     // Ignore .editorconfig file. Only for development.
             '!base/inc/installer/inc/github-plugin-updater.php',  // Exclude Installer's  Updater.
+            '!{package.json,package-lock.json}',                  // Exclude node package files.
         ]
     },
     i18n: {
