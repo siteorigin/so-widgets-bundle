a0cc3f925964942a99b6a567df68eba93b72a02b
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-09-23 23:07:57 +1200

    Order Field: Prevent Site Editor Desync

--- base/inc/fields/js/order-field.js
+++ base/inc/fields/js/order-field.js
@@ -3,9 +3,15 @@
 ( function( $ ) {
 
 	const setupOrderField = function( e ) {
-		const $$ = $( this );
-		const $valField = $$.find( '.siteorigin-widget-input' );
-		const $items = $$.find( '.siteorigin-widget-order-items' );
+		const $field = $( this );
+		if ( $field.attr( 'data-initialized' ) ) {
+			return;
+		}
+
+		$field.attr( 'data-initialized', true );
+
+		const $valField = $field.find( '.siteorigin-widget-input' );
+		const $items = $field.find( '.siteorigin-widget-order-items' );
 
 		$items.sortable( {
 			stop: function( e, ui ) {
@@ -20,13 +26,13 @@
 			}
 		} );
 
-		$$.on( 'change', function( event, params ) {
+		$field.on( 'change', function( event, params ) {
 			if ( ! ( params && params.silent ) ) {
 				const values = $valField.val() === '' ? [] : $valField.val().split( ',' );
 				if ( values.length ) {
 					for ( let i = 0; i < values.length; i++) {
 						const val = values[ i ];
-						const $item = $$.find( '.siteorigin-widget-order-item[data-value=' + val + ']' );
+						const $item = $field.find( '.siteorigin-widget-order-item[data-value=' + val + ']' );
 						$items.append( $item );
 					}
 				}
@@ -36,11 +42,11 @@
 
 	 // If the current page isn't the site editor, set up the Order field now.
 	 if (
-		 window.top === window.self &&
-		 (
-			 typeof pagenow === 'string' &&
-			 pagenow !== 'site-editor'
-		 )
+		window.top === window.self &&
+		(
+			typeof pagenow === 'string' &&
+			pagenow !== 'site-editor'
+		)
 	 ) {
 		 $( document ).on( 'sowsetupformfield', '.siteorigin-widget-field-type-order', setupOrderField );
 	 }
